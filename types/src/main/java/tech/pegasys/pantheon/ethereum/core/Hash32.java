ccf25c4d3c2ccb9e45b58cce0cdfae1469c8876c
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2018-12-26 16:49:36 +0300

    Add Address, Hash32, LogTopic Ethereum data types

---
+++ types/src/main/java/tech/pegasys/pantheon/ethereum/core/Hash32.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.pantheon.ethereum.core;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import tech.pegasys.pantheon.util.bytes.Bytes32;
+import tech.pegasys.pantheon.util.bytes.DelegatingBytes32;
+
+/** A 32-bytes hash value as used in Ethereum blocks, that is the result of the KEC algorithm. */
+public class Hash32 extends DelegatingBytes32 {
+
+  public static final Hash32 ZERO = new Hash32(Bytes32.ZERO);
+
+  private Hash32(final Bytes32 bytes) {
+    super(bytes);
+  }
+
+  public static Hash32 wrap(final Bytes32 bytes) {
+    return new Hash32(bytes);
+  }
+
+  /**
+   * Parse an hexadecimal string representing a hash value.
+   *
+   * @param str An hexadecimal string (with or without the leading '0x') representing a valid hash
+   *     value.
+   * @return The parsed hash.
+   * @throws NullPointerException if the provided string is {@code null}.
+   * @throws IllegalArgumentException if the string is either not hexadecimal, or not the valid
+   *     representation of a hash (not 32 bytes).
+   */
+  @JsonCreator
+  public static Hash32 fromHexString(final String str) {
+    return new Hash32(Bytes32.fromHexStringStrict(str));
+  }
+
+  public static Hash32 fromHexStringLenient(final String str) {
+    return new Hash32(Bytes32.fromHexStringLenient(str));
+  }
+}
