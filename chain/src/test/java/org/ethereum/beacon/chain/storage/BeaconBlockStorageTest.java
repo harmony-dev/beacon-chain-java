91dd31aadfcd4da70f113d4b211e58c01ab5a2c0
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-01-07 17:46:17 +0300

    Add BlockStorage test

---
+++ chain/src/test/java/org/ethereum/beacon/chain/storage/BeaconBlockStorageTest.java
@@ -0,0 +1,231 @@
+package org.ethereum.beacon.chain.storage;
+
+import org.ethereum.beacon.chain.storage.impl.MemChainStorage;
+import org.ethereum.beacon.core.BeaconBlock;
+import org.ethereum.beacon.core.BeaconBlockBody;
+import org.junit.Test;
+import tech.pegasys.artemis.ethereum.core.Hash32;
+import tech.pegasys.artemis.util.bytes.Bytes32;
+import tech.pegasys.artemis.util.bytes.Bytes96;
+import tech.pegasys.artemis.util.bytes.BytesValues;
+import tech.pegasys.artemis.util.uint.UInt64;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+
+public class BeaconBlockStorageTest {
+
+  private long counter = 0;
+
+  private BeaconBlockStorage create() {
+    return new MemChainStorage().createBeaconBlockStorage();
+  }
+
+  private BeaconBlock createBlock(long slot, BeaconBlock parent) {
+    return new BeaconBlock(UInt64.valueOf(slot),
+        parent == null ? Hash32.ZERO : parent.getHash(),
+        Hash32.wrap(Bytes32.leftPad(BytesValues.toMinimalBytes(counter++))),
+        Hash32.ZERO, Hash32.ZERO, Bytes96.ZERO, BeaconBlockBody.EMPTY);
+  }
+
+  @Test
+  public void testEmpty1() throws Exception {
+    BeaconBlockStorage bbs = create();
+    assertTrue(bbs.isEmpty());
+    assertEquals(0, bbs.getSlotBlocks(0).size());
+    assertFalse(bbs.getSlotCanonicalBlock(0).isPresent());
+    assertEquals(-1, bbs.getMaxSlot());
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testEmpty2() throws Exception {
+    BeaconBlockStorage bbs = create();
+    bbs.getCanonicalHead();
+  }
+
+  @Test
+  public void testGenesis1() throws Exception {
+    BeaconBlockStorage bbs = create();
+    BeaconBlock genBlock = createBlock(0, null);
+    bbs.put(genBlock);
+
+    assertFalse(bbs.isEmpty());
+    assertEquals(genBlock.getHash(), bbs.getCanonicalHead());
+    assertEquals(0, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotBlocks(0).get(0));
+    assertEquals(0, bbs.getSlotBlocks(1).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+  }
+
+  @Test
+  public void testSimple1() throws Exception {
+    BeaconBlockStorage bbs = create();
+    BeaconBlock genBlock = createBlock(0, null);
+    bbs.put(genBlock);
+    BeaconBlock b1 = createBlock(1, genBlock);
+    bbs.put(b1);
+
+    assertEquals(1, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(1, bbs.getSlotBlocks(1).size());
+    assertEquals(0, bbs.getSlotBlocks(2).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotBlocks(0).get(0));
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertEquals(b1.getHash(), bbs.getSlotBlocks(1).get(0));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b1.getHash());
+
+    assertEquals(1, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(1, bbs.getSlotBlocks(1).size());
+    assertEquals(0, bbs.getSlotBlocks(2).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotBlocks(0).get(0));
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertEquals(b1.getHash(), bbs.getSlotBlocks(1).get(0));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+  }
+
+  @Test
+  public void testSimple2() throws Exception {
+    BeaconBlockStorage bbs = create();
+    BeaconBlock genBlock = createBlock(0, null);
+    bbs.put(genBlock);
+    BeaconBlock b1 = createBlock(2, genBlock);
+    bbs.put(b1);
+
+    assertEquals(2, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(0, bbs.getSlotBlocks(1).size());
+    assertEquals(1, bbs.getSlotBlocks(2).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotBlocks(0).get(0));
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertEquals(0, bbs.getSlotBlocks(1).size());
+    assertFalse(bbs.getSlotCanonicalBlock(1).isPresent());
+    assertEquals(b1.getHash(), bbs.getSlotBlocks(2).get(0));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(2).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b1.getHash());
+
+    assertEquals(2, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(0, bbs.getSlotBlocks(1).size());
+    assertEquals(1, bbs.getSlotBlocks(2).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotBlocks(0).get(0));
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertEquals(0, bbs.getSlotBlocks(1).size());
+    assertFalse(bbs.getSlotCanonicalBlock(1).isPresent());
+    assertEquals(b1.getHash(), bbs.getSlotBlocks(2).get(0));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(2).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+  }
+
+  @Test
+  public void testFork1() throws Exception {
+    BeaconBlockStorage bbs = create();
+    BeaconBlock genBlock = createBlock(0, null);
+    bbs.put(genBlock);
+    BeaconBlock b1 = createBlock(1, genBlock);
+    bbs.put(b1);
+    BeaconBlock b1_1 = createBlock(1, genBlock);
+    bbs.put(b1_1);
+
+    assertEquals(1, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(2, bbs.getSlotBlocks(1).size());
+    assertEquals(0, bbs.getSlotBlocks(2).size());
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1.getHash()));
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1_1.getHash()));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b1.getHash());
+
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1.getHash()));
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1_1.getHash()));
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b1_1.getHash());
+
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1.getHash()));
+    assertTrue(new HashSet<>(bbs.getSlotBlocks(1)).contains(b1_1.getHash()));
+    assertEquals(b1_1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1_1.getHash(), bbs.getCanonicalHead());
+
+  }
+
+  @Test
+  public void testFork2() throws Exception {
+    BeaconBlockStorage bbs = create();
+    BeaconBlock genBlock = createBlock(0, null);
+    bbs.put(genBlock);
+    BeaconBlock b1 = createBlock(1, genBlock);
+    bbs.put(b1);
+    BeaconBlock b2 = createBlock(2, genBlock);
+    bbs.put(b2);
+
+    assertEquals(2, bbs.getMaxSlot());
+    assertEquals(1, bbs.getSlotBlocks(0).size());
+    assertEquals(1, bbs.getSlotBlocks(1).size());
+    assertEquals(1, bbs.getSlotBlocks(2).size());
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+    assertFalse(bbs.getSlotCanonicalBlock(2).isPresent());
+
+    bbs.reorgTo(b1.getHash());
+
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+    assertFalse(bbs.getSlotCanonicalBlock(2).isPresent());
+
+    bbs.reorgTo(b2.getHash());
+
+    assertEquals(genBlock.getHash(), bbs.getSlotCanonicalBlock(0).get());
+    assertFalse(bbs.getSlotCanonicalBlock(1).isPresent());
+    assertEquals(b2.getHash(), bbs.getSlotCanonicalBlock(2).get());
+    assertEquals(b2.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b1.getHash());
+
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertEquals(b1.getHash(), bbs.getCanonicalHead());
+    assertFalse(bbs.getSlotCanonicalBlock(2).isPresent());
+
+    BeaconBlock b3 = createBlock(3, b1);
+    bbs.put(b3);
+    BeaconBlock b4 = createBlock(4, b2);
+    bbs.put(b4);
+
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertFalse(bbs.getSlotCanonicalBlock(2).isPresent());
+    assertEquals(b3.getHash(), bbs.getSlotCanonicalBlock(3).get());
+    assertFalse(bbs.getSlotCanonicalBlock(4).isPresent());
+    assertFalse(bbs.getSlotCanonicalBlock(5).isPresent());
+    assertEquals(b3.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b4.getHash());
+
+    assertFalse(bbs.getSlotCanonicalBlock(1).isPresent());
+    assertEquals(b2.getHash(), bbs.getSlotCanonicalBlock(2).get());
+    assertFalse(bbs.getSlotCanonicalBlock(3).isPresent());
+    assertEquals(b4.getHash(), bbs.getSlotCanonicalBlock(4).get());
+    assertFalse(bbs.getSlotCanonicalBlock(5).isPresent());
+    assertEquals(b4.getHash(), bbs.getCanonicalHead());
+
+    bbs.reorgTo(b3.getHash());
+
+    assertEquals(b1.getHash(), bbs.getSlotCanonicalBlock(1).get());
+    assertFalse(bbs.getSlotCanonicalBlock(2).isPresent());
+    assertEquals(b3.getHash(), bbs.getSlotCanonicalBlock(3).get());
+    assertFalse(bbs.getSlotCanonicalBlock(4).isPresent());
+    assertFalse(bbs.getSlotCanonicalBlock(5).isPresent());
+    assertEquals(b3.getHash(), bbs.getCanonicalHead());
+  }
+}
