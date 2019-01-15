e8663055b9c317938049480471dac6c7271322db
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-01-15 16:57:05 +0600

    consensus: implement CompositeBlockVerifier

---
+++ consensus/src/main/java/org/ethereum/beacon/consensus/verifier/CompositeBlockVerifier.java
@@ -0,0 +1,58 @@
+package org.ethereum.beacon.consensus.verifier;
+
+import static org.ethereum.beacon.consensus.verifier.VerificationResult.PASSED;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.ethereum.beacon.core.BeaconBlock;
+import org.ethereum.beacon.core.BeaconState;
+
+/**
+ * Aggregates a number of block verifiers.
+ *
+ * <p>Verifiers are triggered according to the order that they were added with. If one of
+ * verification has failed it returns immediately with this failed result.
+ *
+ * @see BeaconBlockVerifier
+ */
+public class CompositeBlockVerifier implements BeaconBlockVerifier {
+
+  private List<BeaconBlockVerifier> verifiers;
+
+  public CompositeBlockVerifier(List<BeaconBlockVerifier> verifiers) {
+    this.verifiers = verifiers;
+  }
+
+  @Override
+  public VerificationResult verify(BeaconBlock block, BeaconState state) {
+    for (BeaconBlockVerifier verifier : verifiers) {
+      VerificationResult result = verifier.verify(block, state);
+      if (result != PASSED) {
+        return result;
+      }
+    }
+    return PASSED;
+  }
+
+  public static class Builder {
+    private List<BeaconBlockVerifier> verifiers;
+
+    private Builder() {
+      this.verifiers = new ArrayList<>();
+    }
+
+    public static Builder createNew() {
+      return new Builder();
+    }
+
+    public Builder with(BeaconBlockVerifier verifier) {
+      this.verifiers.add(verifier);
+      return this;
+    }
+
+    public CompositeBlockVerifier build() {
+      assert verifiers.size() > 0;
+      return new CompositeBlockVerifier(verifiers);
+    }
+  }
+}
