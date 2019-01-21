325130ff9cc338123cf0fbdbc9be2109b08b6535
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-01-21 16:07:19 +0600

    consensus: verify order of deposit operations

--- consensus/src/main/java/org/ethereum/beacon/consensus/verifier/block/DepositListVerifier.java
+++ consensus/src/main/java/org/ethereum/beacon/consensus/verifier/block/DepositListVerifier.java
@@ -1,8 +1,13 @@
 package org.ethereum.beacon.consensus.verifier.block;
 
+import static org.ethereum.beacon.consensus.verifier.VerificationResult.PASSED;
+import static org.ethereum.beacon.consensus.verifier.VerificationResult.failedResult;
+
 import org.ethereum.beacon.consensus.verifier.OperationVerifier;
+import org.ethereum.beacon.consensus.verifier.VerificationResult;
 import org.ethereum.beacon.core.operations.Deposit;
 import org.ethereum.beacon.core.spec.ChainSpec;
+import tech.pegasys.artemis.util.uint.UInt64;
 
 /**
  * Verifies deposit list.
@@ -13,6 +18,23 @@ public class DepositListVerifier extends OperationListVerifier<Deposit> {
 
   public DepositListVerifier(OperationVerifier<Deposit> operationVerifier, ChainSpec chainSpec) {
     super(operationVerifier, block -> block.getBody().getDeposits(), chainSpec.getMaxDeposits());
+
+    addCustomVerifier(
+        deposits -> {
+          if (deposits.size() > 0) {
+            UInt64 expectedIndex = deposits.get(0).getMerkleTreeIndex();
+            for (Deposit deposit : deposits) {
+              if (!deposit.getMerkleTreeIndex().equals(expectedIndex)) {
+                return failedResult(
+                    "inclusion order is broken, expected index %d but got %d",
+                    expectedIndex, deposit.getMerkleTreeIndex());
+              }
+              expectedIndex = expectedIndex.increment();
+            }
+          }
+
+          return PASSED;
+        });
   }
 
   @Override
