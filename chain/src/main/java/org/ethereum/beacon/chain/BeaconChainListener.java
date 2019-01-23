09057a9f3073ed845eab185dfe882348912da868
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-01-23 18:00:44 +0600

    chain: get rid of BeaconChainListener

--- chain/src/main/java/org/ethereum/beacon/chain/BeaconChainListener.java
+++
@@ -1,8 +0,0 @@
-package org.ethereum.beacon.chain;
-
-import java.util.concurrent.CompletableFuture;
-
-/** A stub for beacon chain listener. */
-public interface BeaconChainListener {
-  CompletableFuture<Void> getSyncDone();
-}
