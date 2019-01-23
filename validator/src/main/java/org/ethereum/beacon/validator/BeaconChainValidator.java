9ff3c1ff51365f334ff82ef48cc69d2283ca402f
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-01-23 20:56:07 +0600

    validator: run attester task right after state received

--- validator/src/main/java/org/ethereum/beacon/validator/BeaconChainValidator.java
+++ validator/src/main/java/org/ethereum/beacon/validator/BeaconChainValidator.java
@@ -2,7 +2,6 @@ package org.ethereum.beacon.validator;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import org.ethereum.beacon.chain.observer.ObservableBeaconState;
 import org.ethereum.beacon.consensus.SpecHelpers;
@@ -13,7 +12,6 @@ import org.ethereum.beacon.validator.crypto.MessageSigner;
 import tech.pegasys.artemis.ethereum.core.Hash32;
 import tech.pegasys.artemis.util.bytes.Bytes96;
 import tech.pegasys.artemis.util.uint.UInt24;
-import tech.pegasys.artemis.util.uint.UInt64;
 
 /** Runs a single validator in the same instance with chain processing. */
 public class BeaconChainValidator implements ValidatorService {
@@ -26,7 +24,6 @@ public class BeaconChainValidator implements ValidatorService {
 
   private ScheduledExecutorService executor;
 
-  private ObservableBeaconState recentState;
   private Hash32 recentDepositRoot;
   private UInt24 index = UInt24.MAX_VALUE;
 
@@ -53,6 +50,7 @@ public class BeaconChainValidator implements ValidatorService {
               return t;
             });
     subscribeToObservableStateUpdates(this::processState);
+    subscribeToDepositRootUpdates(this::processDepositRoot);
   }
 
   @Override
@@ -69,8 +67,6 @@ public class BeaconChainValidator implements ValidatorService {
       return;
     }
 
-    this.recentState = state;
-
     if (!isInitialized()) {
       init(state.getLatestSlotState());
     }
@@ -84,39 +80,31 @@ public class BeaconChainValidator implements ValidatorService {
     this.recentDepositRoot = depositRoot;
   }
 
-  private void runTasks(ObservableBeaconState observableState) {
+  private void runTasks(final ObservableBeaconState observableState) {
     BeaconState state = observableState.getLatestSlotState();
     UInt24 proposerIndex = specHelpers.get_beacon_proposer_index(state, state.getSlot());
     if (index.equals(proposerIndex)) {
-      propose(observableState);
+      runAsync(() -> propose(observableState));
     } else if (specHelpers.is_in_beacon_chain_committee(state, state.getSlot(), index)) {
-      attest(observableState);
+      runAsync(() -> attest(observableState));
     }
+  }
+
+  private void runAsync(Runnable routine) {
+    executor.execute(routine);
   }
 
   private void propose(final ObservableBeaconState observableState) {
     if (recentDepositRoot != null) {
-      executor.execute(
-          () -> {
-            final BeaconBlock newBlock =
-                proposer.propose(index, observableState, recentDepositRoot, messageSigner);
-            propagateBlock(newBlock);
-          });
+      BeaconBlock newBlock =
+          proposer.propose(index, observableState, recentDepositRoot, messageSigner);
+      propagateBlock(newBlock);
     }
   }
 
   private void attest(final ObservableBeaconState observableState) {
-    BeaconState state = observableState.getLatestSlotState();
-    UInt64 slotMiddleTime = specHelpers.get_slot_middle_time(state, state.getSlot());
-    long slotMiddleTimeMillis = 1000 * slotMiddleTime.getValue();
-
-    executor.schedule(
-        () -> {
-          final Attestation attestation = attester.attest(this.recentState);
-          propagateAttestation(attestation);
-        },
-        Math.max(0, System.currentTimeMillis() - slotMiddleTimeMillis),
-        TimeUnit.MILLISECONDS);
+    Attestation attestation = attester.attest(observableState);
+    propagateAttestation(attestation);
   }
 
   private boolean isInitialized() {
@@ -130,5 +118,5 @@ public class BeaconChainValidator implements ValidatorService {
 
   private void subscribeToObservableStateUpdates(Consumer<ObservableBeaconState> payload) {}
 
-  private void subsribeToDepositRootUpdates(Consumer<Hash32> payload) {}
+  private void subscribeToDepositRootUpdates(Consumer<Hash32> payload) {}
 }
