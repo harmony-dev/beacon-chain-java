5e41b76b3d71ed7cb435f164d60f56d3dd622013
Dmitry Shmatko <zilm@zilm.net>
2019-01-25 17:17:13 +0300

    chain: get rid from attestation cache in LMD Ghost implementation

--- chain/src/main/java/org/ethereum/beacon/chain/LMDGhostHeadFunction.java
+++ chain/src/main/java/org/ethereum/beacon/chain/LMDGhostHeadFunction.java
@@ -9,17 +9,8 @@ import org.ethereum.beacon.core.BeaconBlock;
 import org.ethereum.beacon.core.BeaconState;
 import org.ethereum.beacon.core.operations.Attestation;
 import org.ethereum.beacon.core.state.ValidatorRecord;
-import reactor.core.publisher.Flux;
 import tech.pegasys.artemis.ethereum.core.Hash32;
-import tech.pegasys.artemis.util.bytes.Bytes48;
-import tech.pegasys.artemis.util.uint.UInt24;
-import tech.pegasys.artemis.util.uint.UInt64;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 
 /**
@@ -33,28 +24,17 @@ public class LMDGhostHeadFunction implements HeadFunction {
   private final BeaconBlockStorage blockStorage;
   private final BeaconStateStorage stateStorage;
   private final SpecHelpers specHelpers;
+  Function<ValidatorRecord, Attestation> latestAttestationStorage;
   private final int SEARCH_LIMIT = Integer.MAX_VALUE;
-  private final Map<Bytes48, Attestation> attestationCache = new HashMap<>();
-  private final Map<UInt64, Set<Bytes48>> validatorSlotCache = new HashMap<>();
-  private BeaconState state;
 
   public LMDGhostHeadFunction(
-      BeaconChain beaconChain, BeaconChainStorage chainStorage, SpecHelpers specHelpers) {
+      BeaconChainStorage chainStorage,
+      Function<ValidatorRecord, Attestation> latestAttestationStorage,
+      SpecHelpers specHelpers) {
     this.stateStorage = chainStorage.getBeaconStateStorage();
     this.blockStorage = chainStorage.getBeaconBlockStorage();
+    this.latestAttestationStorage = latestAttestationStorage;
     this.specHelpers = specHelpers;
-    Optional<BeaconBlock> justifiedBlock =
-        blockStorage.getJustifiedBlock(blockStorage.getMaxSlot(), SEARCH_LIMIT);
-    this.state =
-        justifiedBlock
-            .flatMap(block -> chainStorage.getBeaconStateStorage().get(block.getHash()))
-            .orElseThrow(
-                () ->
-                    new RuntimeException(
-                        "State not found for justified block + " + justifiedBlock));
-    Flux.from(beaconChain.getBlockStatesStream())
-        .doOnNext(beaconTuple -> state = beaconTuple.getState())
-        .subscribe();
   }
 
   @Override
@@ -86,55 +66,6 @@ public class LMDGhostHeadFunction implements HeadFunction {
    * first.
    */
   private Attestation get_latest_attestation(ValidatorRecord validatorRecord) {
-    return attestationCache.get(validatorRecord.getPubKey());
-  }
-
-  /**
-   * This should be implemented via some kind of subscription we should be subscribed to all
-   * verified attestations, even those not included in blocks yet
-   */
-  public void addAttestation(Attestation attestation) {
-    List<UInt24> participants =
-        specHelpers.get_attestation_participants(
-            state, attestation.getData(), attestation.getParticipationBitfield());
-
-    List<Bytes48> pubKeys = specHelpers.mapIndicesToPubKeys(state, participants);
-
-    for (Bytes48 pubKey : pubKeys) {
-      if (attestationCache.containsKey(pubKey)) {
-        Attestation oldAttestation = attestationCache.get(pubKey);
-        if (attestation.getData().getSlot().compareTo(oldAttestation.getData().getSlot()) > 0) {
-          attestationCache.put(pubKey, attestation);
-          validatorSlotCache.get(oldAttestation.getData().getSlot()).remove(pubKey);
-          addToSlotCache(attestation.getData().getSlot(), pubKey);
-        } else {
-          // XXX: If several such attestations exist, use the one the validator v observed first
-          // so no need to swap it
-        }
-      } else {
-        attestationCache.put(pubKey, attestation);
-        addToSlotCache(attestation.getData().getSlot(), pubKey);
-      }
-    }
-  }
-
-  private void addToSlotCache(UInt64 slot, Bytes48 pubKey) {
-    if (validatorSlotCache.containsKey(slot)) {
-      validatorSlotCache.get(slot).add(pubKey);
-    } else {
-      Set<Bytes48> pubKeysSet = new HashSet<>();
-      pubKeysSet.add(pubKey);
-      validatorSlotCache.put(slot, pubKeysSet);
-    }
-  }
-
-  /** Purges all entries for slot and before */
-  public void purgeAttestations(UInt64 slot) {
-    for (Map.Entry<UInt64, Set<Bytes48>> entry : validatorSlotCache.entrySet()) {
-      if (entry.getKey().compareTo(slot) <= 0) {
-        entry.getValue().forEach(attestationCache::remove);
-        validatorSlotCache.remove(entry.getKey());
-      }
-    }
+    return latestAttestationStorage.apply(validatorRecord);
   }
 }
