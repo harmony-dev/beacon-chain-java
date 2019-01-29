6ae5657259c3e86b3dd6f882284554ffe5ad4448
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-01-29 15:44:46 +0300

    Fix NextSlotTransitionTest: valid deposit signatures required now

--- consensus/src/test/java/org/ethereum/beacon/consensus/transition/NextSlotTransitionTest.java
+++ consensus/src/test/java/org/ethereum/beacon/consensus/transition/NextSlotTransitionTest.java
@@ -1,31 +1,18 @@
 package org.ethereum.beacon.consensus.transition;
 
-import java.util.Collections;
-import java.util.Collections;
-import java.util.Optional;
+import java.util.List;
+import java.util.Random;
 import org.ethereum.beacon.consensus.SpecHelpers;
+import org.ethereum.beacon.consensus.TestUtils;
 import org.ethereum.beacon.core.BeaconBlocks;
-import org.ethereum.beacon.core.BeaconState;
 import org.ethereum.beacon.core.operations.Deposit;
-import org.ethereum.beacon.core.operations.deposit.DepositData;
-import org.ethereum.beacon.core.operations.deposit.DepositInput;
 import org.ethereum.beacon.core.spec.ChainSpec;
-import org.ethereum.beacon.pow.AbstractDepositContract;
 import org.ethereum.beacon.core.state.Eth1Data;
-import org.ethereum.beacon.pow.DepositContract;
 import org.ethereum.beacon.pow.DepositContract.ChainStart;
 import org.junit.Assert;
 import org.junit.Test;
 import tech.pegasys.artemis.ethereum.core.Hash32;
-import tech.pegasys.artemis.util.bytes.Bytes48;
-import tech.pegasys.artemis.util.bytes.Bytes96;
 import tech.pegasys.artemis.util.uint.UInt64;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class NextSlotTransitionTest {
 
@@ -34,22 +21,20 @@ public class NextSlotTransitionTest {
     Random rnd = new Random();
     UInt64 genesisTime = UInt64.random(rnd);
     Eth1Data eth1Data = new Eth1Data(Hash32.random(rnd), Hash32.random(rnd));
-    ChainSpec chainSpec = ChainSpec.DEFAULT;
+    ChainSpec chainSpec = new ChainSpec.DefaultChainSpec() {
+      @Override
+      public UInt64 getEpochLength() {
+        return UInt64.valueOf(8);
+      }
+    };
+    SpecHelpers specHelpers = new SpecHelpers(chainSpec);
 
-    List<Deposit> deposits = new ArrayList<>();
-    for (int i = 0; i < 8000; i++) {
-      Deposit deposit = new Deposit(Collections.singletonList(Hash32.random(rnd)), UInt64.ZERO,
-          new DepositData(
-              new DepositInput(
-                  Bytes48.intToBytes48(i), Hash32.random(rnd), Bytes96.ZERO
-              ), chainSpec.getMaxDeposit().toGWei(), UInt64.ZERO));
-      deposits.add(deposit);
-    }
+    List<Deposit> deposits = TestUtils.getAnyDeposits(specHelpers, 8).getValue0();
 
     InitialStateTransition initialStateTransition =
         new InitialStateTransition(
             new ChainStart(genesisTime, eth1Data, deposits),
-            new SpecHelpers(chainSpec));
+            specHelpers);
 
     BeaconStateEx initialState =
         initialStateTransition.apply(BeaconBlocks.createGenesis(chainSpec));
--- core/src/main/java/org/ethereum/beacon/core/spec/ChainSpec.java
+++ core/src/main/java/org/ethereum/beacon/core/spec/ChainSpec.java
@@ -16,191 +16,192 @@ public interface ChainSpec
         MaxOperationsPerBlock,
         HonestValidatorParameters {
 
-  ChainSpec DEFAULT =
-      new ChainSpec() {
-        @Override
-        public UInt64 getShardCount() {
-          return SHARD_COUNT;
-        }
+  ChainSpec DEFAULT = new DefaultChainSpec();
 
-        @Override
-        public UInt24 getTargetCommitteeSize() {
-          return TARGET_COMMITTEE_SIZE;
-        }
+  class DefaultChainSpec implements ChainSpec {
+    @Override
+    public UInt64 getShardCount() {
+      return SHARD_COUNT;
+    }
 
-        @Override
-        public Ether getEjectionBalance() {
-          return EJECTION_BALANCE;
-        }
+    @Override
+    public UInt24 getTargetCommitteeSize() {
+      return TARGET_COMMITTEE_SIZE;
+    }
 
-        @Override
-        public UInt64 getMaxBalanceChurnQuotient() {
-          return MAX_BALANCE_CHURN_QUOTIENT;
-        }
+    @Override
+    public Ether getEjectionBalance() {
+      return EJECTION_BALANCE;
+    }
 
-        @Override
-        public UInt64 getBeaconChainShardNumber() {
-          return BEACON_CHAIN_SHARD_NUMBER;
-        }
+    @Override
+    public UInt64 getMaxBalanceChurnQuotient() {
+      return MAX_BALANCE_CHURN_QUOTIENT;
+    }
 
-        @Override
-        public int getMaxCasperVotes() {
-          return MAX_CASPER_VOTES;
-        }
+    @Override
+    public UInt64 getBeaconChainShardNumber() {
+      return BEACON_CHAIN_SHARD_NUMBER;
+    }
 
-        @Override
-        public UInt64 getLatestBlockRootsLength() {
-          return LATEST_BLOCK_ROOTS_LENGTH;
-        }
+    @Override
+    public int getMaxCasperVotes() {
+      return MAX_CASPER_VOTES;
+    }
 
-        @Override
-        public UInt64 getLatestRandaoMixesLength() {
-          return LATEST_RANDAO_MIXES_LENGTH;
-        }
+    @Override
+    public UInt64 getLatestBlockRootsLength() {
+      return LATEST_BLOCK_ROOTS_LENGTH;
+    }
 
-        @Override
-        public UInt64 getLatestPenalizedExitLength() {
-          return LATEST_PENALIZED_EXIT_LENGTH;
-        }
+    @Override
+    public UInt64 getLatestRandaoMixesLength() {
+      return LATEST_RANDAO_MIXES_LENGTH;
+    }
 
-        @Override
-        public UInt64 getMaxWithdrawalsPerEpoch() {
-          return MAX_WITHDRAWALS_PER_EPOCH;
-        }
+    @Override
+    public UInt64 getLatestPenalizedExitLength() {
+      return LATEST_PENALIZED_EXIT_LENGTH;
+    }
 
-        @Override
-        public Address getDepositContractAddress() {
-          return DEPOSIT_CONTRACT_ADDRESS;
-        }
+    @Override
+    public UInt64 getMaxWithdrawalsPerEpoch() {
+      return MAX_WITHDRAWALS_PER_EPOCH;
+    }
 
-        @Override
-        public UInt64 getDepositContractTreeDepth() {
-          return DEPOSIT_CONTRACT_TREE_DEPTH;
-        }
+    @Override
+    public Address getDepositContractAddress() {
+      return DEPOSIT_CONTRACT_ADDRESS;
+    }
 
-        @Override
-        public Ether getMinDeposit() {
-          return MIN_DEPOSIT;
-        }
+    @Override
+    public UInt64 getDepositContractTreeDepth() {
+      return DEPOSIT_CONTRACT_TREE_DEPTH;
+    }
 
-        @Override
-        public Ether getMaxDeposit() {
-          return MAX_DEPOSIT;
-        }
+    @Override
+    public Ether getMinDeposit() {
+      return MIN_DEPOSIT;
+    }
 
-        @Override
-        public UInt64 getGenesisForkVersion() {
-          return GENESIS_FORK_VERSION;
-        }
+    @Override
+    public Ether getMaxDeposit() {
+      return MAX_DEPOSIT;
+    }
 
-        @Override
-        public UInt64 getGenesisSlot() {
-          return GENESIS_SLOT;
-        }
+    @Override
+    public UInt64 getGenesisForkVersion() {
+      return GENESIS_FORK_VERSION;
+    }
 
-        @Override
-        public UInt64 getGenesisStartShard() {
-          return GENESIS_START_SHARD;
-        }
+    @Override
+    public UInt64 getGenesisSlot() {
+      return GENESIS_SLOT;
+    }
 
-        @Override
-        public UInt64 getFarFutureSlot() {
-          return FAR_FUTURE_SLOT;
-        }
+    @Override
+    public UInt64 getGenesisStartShard() {
+      return GENESIS_START_SHARD;
+    }
 
-        @Override
-        public Bytes96 getEmptySignature() {
-          return EMPTY_SIGNATURE;
-        }
+    @Override
+    public UInt64 getFarFutureSlot() {
+      return FAR_FUTURE_SLOT;
+    }
 
-        @Override
-        public Bytes1 getBlsWithdrawalPrefixByte() {
-          return BLS_WITHDRAWAL_PREFIX_BYTE;
-        }
+    @Override
+    public Bytes96 getEmptySignature() {
+      return EMPTY_SIGNATURE;
+    }
 
-        @Override
-        public UInt64 getSlotDuration() {
-          return SLOT_DURATION;
-        }
+    @Override
+    public Bytes1 getBlsWithdrawalPrefixByte() {
+      return BLS_WITHDRAWAL_PREFIX_BYTE;
+    }
 
-        @Override
-        public UInt64 getMinAttestationInclusionDelay() {
-          return MIN_ATTESTATION_INCLUSION_DELAY;
-        }
+    @Override
+    public UInt64 getSlotDuration() {
+      return SLOT_DURATION;
+    }
 
-        @Override
-        public UInt64 getEpochLength() {
-          return EPOCH_LENGTH;
-        }
+    @Override
+    public UInt64 getMinAttestationInclusionDelay() {
+      return MIN_ATTESTATION_INCLUSION_DELAY;
+    }
 
-        @Override
-        public UInt64 getSeedLookahead() {
-          return SEED_LOOKAHEAD;
-        }
+    @Override
+    public UInt64 getEpochLength() {
+      return EPOCH_LENGTH;
+    }
 
-        @Override
-        public UInt64 getEntryExitDelay() {
-          return ENTRY_EXIT_DELAY;
-        }
+    @Override
+    public UInt64 getSeedLookahead() {
+      return SEED_LOOKAHEAD;
+    }
 
-        @Override
-        public UInt64 getEth1DataVotingPeriod() {
-          return ETH1_DATA_VOTING_PERIOD;
-        }
+    @Override
+    public UInt64 getEntryExitDelay() {
+      return ENTRY_EXIT_DELAY;
+    }
 
-        @Override
-        public UInt64 getMinValidatorWithdrawalTime() {
-          return MIN_VALIDATOR_WITHDRAWAL_TIME;
-        }
+    @Override
+    public UInt64 getEth1DataVotingPeriod() {
+      return ETH1_DATA_VOTING_PERIOD;
+    }
 
-        @Override
-        public UInt64 getBaseRewardQuotient() {
-          return BASE_REWARD_QUOTIENT;
-        }
+    @Override
+    public UInt64 getMinValidatorWithdrawalTime() {
+      return MIN_VALIDATOR_WITHDRAWAL_TIME;
+    }
 
-        @Override
-        public UInt64 getWhistleblowerRewardQuotient() {
-          return WHISTLEBLOWER_REWARD_QUOTIENT;
-        }
+    @Override
+    public UInt64 getBaseRewardQuotient() {
+      return BASE_REWARD_QUOTIENT;
+    }
 
-        @Override
-        public UInt64 getIncluderRewardQuotient() {
-          return INCLUDER_REWARD_QUOTIENT;
-        }
+    @Override
+    public UInt64 getWhistleblowerRewardQuotient() {
+      return WHISTLEBLOWER_REWARD_QUOTIENT;
+    }
 
-        @Override
-        public UInt64 getInactivityPenaltyQuotient() {
-          return INACTIVITY_PENALTY_QUOTIENT;
-        }
+    @Override
+    public UInt64 getIncluderRewardQuotient() {
+      return INCLUDER_REWARD_QUOTIENT;
+    }
 
-        @Override
-        public int getMaxProposerSlashings() {
-          return MAX_PROPOSER_SLASHINGS;
-        }
+    @Override
+    public UInt64 getInactivityPenaltyQuotient() {
+      return INACTIVITY_PENALTY_QUOTIENT;
+    }
 
-        @Override
-        public int getMaxCasperSlashings() {
-          return MAX_CASPER_SLASHINGS;
-        }
+    @Override
+    public int getMaxProposerSlashings() {
+      return MAX_PROPOSER_SLASHINGS;
+    }
 
-        @Override
-        public int getMaxAttestations() {
-          return MAX_ATTESTATIONS;
-        }
+    @Override
+    public int getMaxCasperSlashings() {
+      return MAX_CASPER_SLASHINGS;
+    }
 
-        @Override
-        public int getMaxDeposits() {
-          return MAX_DEPOSITS;
-        }
+    @Override
+    public int getMaxAttestations() {
+      return MAX_ATTESTATIONS;
+    }
 
-        @Override
-        public int getMaxExits() {
-          return MAX_EXITS;
-        }
+    @Override
+    public int getMaxDeposits() {
+      return MAX_DEPOSITS;
+    }
 
-        @Override
-        public long getEth1FollowDistance() {
-          return ETH1_FOLLOW_DISTANCE;
-        }
-      };
+    @Override
+    public int getMaxExits() {
+      return MAX_EXITS;
+    }
+
+    @Override
+    public long getEth1FollowDistance() {
+      return ETH1_FOLLOW_DISTANCE;
+    }
+  };
 }
