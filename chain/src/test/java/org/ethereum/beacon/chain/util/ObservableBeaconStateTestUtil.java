6ea699bb93e2f297e3610156a7f709e2a7c4dcfd
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-02-06 18:52:06 +0600

    validator: test BeaconChainValidator service

--- chain/src/test/java/org/ethereum/beacon/chain/util/ObservableBeaconStateTestUtil.java
+++ chain/src/test/java/org/ethereum/beacon/chain/util/ObservableBeaconStateTestUtil.java
@@ -9,7 +9,9 @@ import org.ethereum.beacon.consensus.transition.InitialStateTransition;
 import org.ethereum.beacon.core.BeaconBlock;
 import org.ethereum.beacon.core.BeaconBlocks;
 import org.ethereum.beacon.core.BeaconState;
+import org.ethereum.beacon.core.MutableBeaconState;
 import org.ethereum.beacon.core.state.Eth1Data;
+import org.ethereum.beacon.core.types.SlotNumber;
 import org.ethereum.beacon.core.types.Time;
 import org.ethereum.beacon.pow.DepositContract.ChainStart;
 import tech.pegasys.artemis.ethereum.core.Hash32;
@@ -19,6 +21,21 @@ public class ObservableBeaconStateTestUtil {
   public static ObservableBeaconState createInitialState(Random random, SpecHelpers specHelpers) {
     return createInitialState(
         random, specHelpers, PendingOperationsTestUtil.createEmptyPendingOperations());
+  }
+
+  public static ObservableBeaconState createInitialState(
+      Random random, SpecHelpers specHelpers, SlotNumber slotNumber) {
+    ObservableBeaconState originalState =
+        createInitialState(
+            random, specHelpers, PendingOperationsTestUtil.createEmptyPendingOperations());
+
+    MutableBeaconState modifiedState = originalState.getLatestSlotState().createMutableCopy();
+    modifiedState.setSlot(slotNumber);
+
+    BeaconBlock modifiedHead =
+        BeaconBlock.Builder.fromBlock(originalState.getHead()).withSlot(slotNumber).build();
+    return new ObservableBeaconState(
+        modifiedHead, modifiedState, originalState.getPendingOperations());
   }
 
   public static ObservableBeaconState createInitialState(
