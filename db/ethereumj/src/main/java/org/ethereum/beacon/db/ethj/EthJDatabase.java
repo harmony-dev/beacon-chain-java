31459d15386f84f39f55678c46f02878d7a98664
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-02-10 17:07:45 +0600

    Implement in memory database

--- db/ethereumj/src/main/java/org/ethereum/beacon/db/ethj/EthJDatabase.java
+++ db/ethereumj/src/main/java/org/ethereum/beacon/db/ethj/EthJDatabase.java
@@ -1,33 +1,22 @@
 package org.ethereum.beacon.db.ethj;
 
-import org.ethereum.beacon.db.Database;
+import java.util.function.Function;
+import org.ethereum.beacon.db.XorKeyDatabase;
 import org.ethereum.beacon.db.source.CodecSource;
-import org.ethereum.beacon.db.source.DataSource;
-import org.ethereum.beacon.db.source.impl.XorDataSource;
 import org.ethereum.datasource.Source;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 
-import java.util.function.Function;
+public class EthJDatabase extends XorKeyDatabase {
 
-public class EthJDatabase implements Database {
-
-  private final DataSource<BytesValue, BytesValue> backingDataSource;
-  private final Function<BytesValue, BytesValue> sourceNameHasher;
-
-  public EthJDatabase(Source<byte[], byte[]> ethJDataSource,
-                      Function<BytesValue, BytesValue> sourceNameHasher) {
-    this.sourceNameHasher = sourceNameHasher;
-    EthJDataSourceAdapter<byte[], byte[]> adapter = new EthJDataSourceAdapter<>(ethJDataSource);
-    backingDataSource = new CodecSource<>(adapter,
-        BytesValue::extractArray,
-        BytesValue::extractArray,
-        BytesValue::wrap);
-  }
-
-  @Override
-  public DataSource<BytesValue, BytesValue> createStorage(String name) {
-    return new XorDataSource<>(backingDataSource,
-        sourceNameHasher.apply(BytesValue.wrap(name.getBytes())));
+  public EthJDatabase(
+      Source<byte[], byte[]> ethJDataSource, Function<BytesValue, BytesValue> sourceNameHasher) {
+    super(
+        new CodecSource<>(
+            new EthJDataSourceAdapter<>(ethJDataSource),
+            BytesValue::extractArray,
+            BytesValue::extractArray,
+            BytesValue::wrap),
+        sourceNameHasher);
   }
 
   @Override
