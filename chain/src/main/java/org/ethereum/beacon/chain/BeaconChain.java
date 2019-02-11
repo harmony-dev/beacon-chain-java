25afd7b31880236126dc6410b946afb671f713dd
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-02-11 21:25:54 +0300

    Move getRecentlyProcessed() method from Mutable to BeaconChain

--- chain/src/main/java/org/ethereum/beacon/chain/BeaconChain.java
+++ chain/src/main/java/org/ethereum/beacon/chain/BeaconChain.java
@@ -1,12 +1,23 @@
 package org.ethereum.beacon.chain;
 
+import java.time.Duration;
 import org.ethereum.beacon.chain.storage.BeaconTuple;
 import org.ethereum.beacon.core.BeaconState;
 import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
 
 public interface BeaconChain {
 
   Publisher<BeaconTuple> getBlockStatesStream();
+
+  /**
+   * Returns the most recent processed tuple.
+   *
+   * <p><strong>Note:</strong> it's not necessary a chain head.
+   *
+   * @return recently processed tuple.
+   */
+  BeaconTuple getRecentlyProcessed();
 
   void init();
 }
