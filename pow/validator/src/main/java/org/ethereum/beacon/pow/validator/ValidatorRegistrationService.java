a8757a8dba01140149ba8992a4d14f9f8e51aaeb
Dmitrii Shmatko <leodex23@gmail.com>
2019-02-11 23:59:57 +0300

    pow/validator: finish validator registration service without stage storing to DB

--- pow/validator/src/main/java/org/ethereum/beacon/pow/validator/ValidatorRegistrationService.java
+++ pow/validator/src/main/java/org/ethereum/beacon/pow/validator/ValidatorRegistrationService.java
@@ -1,128 +1,18 @@
 package org.ethereum.beacon.pow.validator;
 
-import org.ethereum.beacon.chain.observer.ObservableBeaconState;
-import org.ethereum.beacon.consensus.SpecHelpers;
-import org.ethereum.beacon.core.BeaconState;
-import org.ethereum.beacon.core.operations.deposit.DepositInput;
-import org.ethereum.beacon.core.types.BLSPubkey;
-import org.ethereum.beacon.core.types.BLSSignature;
-import org.ethereum.beacon.core.types.Gwei;
-import org.ethereum.beacon.crypto.Hashes;
-import org.ethereum.beacon.pow.DepositContract;
-import org.ethereum.beacon.ssz.Hasher;
-import org.ethereum.beacon.ssz.SSZHasher;
-import org.ethereum.beacon.ssz.Serializer;
-import org.ethereum.beacon.validator.crypto.MessageSigner;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import tech.pegasys.artemis.ethereum.core.Address;
-import tech.pegasys.artemis.ethereum.core.Hash32;
-import tech.pegasys.artemis.util.bytes.Bytes8;
-import tech.pegasys.artemis.util.bytes.BytesValue;
+/**
+ * Registers validator in DepositContract and passes all way to active validator according to spec
+ * <a
+ * href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/validator/0_beacon-chain-validator.md">https://github.com/ethereum/eth2.0-specs/blob/dev/specs/validator/0_beacon-chain-validator.md</a>
+ */
+public interface ValidatorRegistrationService {
+  void start();
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Function;
-
-import static org.ethereum.beacon.core.spec.SignatureDomains.DEPOSIT;
-
-public class ValidatorRegistrationService {
-  TransactionBuilder transactionBuilder;
-  TransactionGateway transactionGateway;
-  DepositContract depositContract;
-  SpecHelpers specHelpers;
-  Hasher<Hash32> sszHasher;
-  Serializer sszSerializer;
-  BeaconState latestState;
-  Publisher<ObservableBeaconState> observablePublisher;
-
-  // Validator
-  MessageSigner<BLSSignature> signer;
-  BLSPubkey pubKey;
-  Hash32 withdrawalCredentials;
-
-  private ScheduledExecutorService executor;
-
-  public ValidatorRegistrationService(
-      TransactionBuilder transactionBuilder,
-      TransactionGateway transactionGateway,
-      Publisher<ObservableBeaconState> observablePublisher,
-      SpecHelpers specHelpers) {
-    this.transactionBuilder = transactionBuilder;
-    this.transactionGateway = transactionGateway;
-    this.observablePublisher = observablePublisher;
-    this.specHelpers = specHelpers;
-    Function<BytesValue, BytesValue> hashingFunction =
-        (data) ->
-            BytesValue.wrap(
-                Hashes.keccak256(BytesValue.of(data.getArrayUnsafe())).getArrayUnsafe());
-    sszHasher = SSZHasher.simpleHasher(hashingFunction);
-    sszSerializer = Serializer.annotationSerializer();
-  }
-
-  public void start() {
-    // TODO: use me
-    this.executor =
-        Executors.newSingleThreadScheduledExecutor(
-            runnable -> {
-              Thread t = new Thread(runnable, "validator-registration-service");
-              t.setDaemon(true);
-              return t;
-            });
-    Flux.from(observablePublisher).subscribe(o -> latestState = o.getLatestSlotState());
-    // TODO: issue 1: if we have only amount we couldn't be sure it was this exact deposit
-    // easy solution: only full deposit for beginning
-    // TODO: issue 2: where to get old DepositData? We either should keep some state in local db/or
-    // use state to check all validators for inclusion
-
-    // TODO: understand current stage
-
-    // TODO: run next stage
-  }
-
-  public CompletableFuture<TransactionGateway.TxStatus> submitDeposit(
-      Gwei amount, Address eth1From, BytesValue eth1PrivKey) {
-    DepositInput depositInput = createDepositInput();
-    BytesValue tx = createTransaction(eth1From, eth1PrivKey, depositInput, amount);
-    return transactionGateway.send(tx);
-  }
-
-  private DepositInput createDepositInput() {
-    // To submit a deposit:
-    //
-    //    Pack the validator's initialization parameters into deposit_input, a DepositInput SSZ
-    // object.
-    //    Set deposit_input.proof_of_possession = EMPTY_SIGNATURE.
-    DepositInput preDepositInput =
-        new DepositInput(pubKey, withdrawalCredentials, BLSSignature.ZERO);
-    // Let proof_of_possession be the result of bls_sign of the hash_tree_root(deposit_input) with
-    // domain=DOMAIN_DEPOSIT.
-    Hash32 hash = sszHasher.calc(preDepositInput);
-    Bytes8 domain =
-        specHelpers.get_domain(
-            latestState.getForkData(), specHelpers.get_current_epoch(latestState), DEPOSIT);
-    BLSSignature signature = signer.sign(hash, domain);
-    // Set deposit_input.proof_of_possession = proof_of_possession.
-    DepositInput depositInput = new DepositInput(pubKey, withdrawalCredentials, signature);
-
-    return depositInput;
-  }
-
-  public BytesValue createTransaction(
-      Address eth1From, BytesValue eth1PrivKey, DepositInput depositInput, Gwei amount) {
-    // Let amount be the amount in Gwei to be deposited by the validator where MIN_DEPOSIT_AMOUNT <=
-    // amount <= MAX_DEPOSIT_AMOUNT.
-    // TODO: shouldn't be asserts
-    assert amount.compareTo(specHelpers.getChainSpec().getMinDepositAmount()) >= 0;
-    assert amount.compareTo(specHelpers.getChainSpec().getMaxDepositAmount()) <= 0;
-    // Send a transaction on the Ethereum 1.0 chain to DEPOSIT_CONTRACT_ADDRESS executing deposit
-    // along with serialize(deposit_input) as the singular bytes input along with a deposit amount
-    // in Gwei.
-    BytesValue unsignedTx =
-        transactionBuilder.createTransaction(
-            eth1From.toString(), sszSerializer.encode2(depositInput), amount);
-
-    return transactionBuilder.signTransaction(unsignedTx, eth1PrivKey);
+  enum RegistrationStage {
+    SEND_TX, // Send Deposit in Eth1
+    AWAIT_INCLUSION, // Await processing of DepositContract root with our validator by smb
+    AWAIT_ACTIVATION, // Await activation epoch
+    VALIDATOR_START, // Start validator proposer / attestation services
+    COMPLETE // We are over
   }
 }
---
+++ pow/validator/src/main/java/org/ethereum/beacon/pow/validator/ValidatorRegistrationServiceImpl.java
@@ -0,0 +1,281 @@
+package org.ethereum.beacon.pow.validator;
+
+import org.ethereum.beacon.chain.observer.ObservableBeaconState;
+import org.ethereum.beacon.consensus.BlockTransition;
+import org.ethereum.beacon.consensus.SpecHelpers;
+import org.ethereum.beacon.consensus.StateTransition;
+import org.ethereum.beacon.consensus.hasher.ObjectHasher;
+import org.ethereum.beacon.consensus.hasher.SSZObjectHasher;
+import org.ethereum.beacon.consensus.transition.BeaconStateEx;
+import org.ethereum.beacon.consensus.transition.PerBlockTransition;
+import org.ethereum.beacon.consensus.transition.PerEpochTransition;
+import org.ethereum.beacon.core.BeaconState;
+import org.ethereum.beacon.core.operations.Deposit;
+import org.ethereum.beacon.core.operations.deposit.DepositInput;
+import org.ethereum.beacon.core.state.ValidatorRecord;
+import org.ethereum.beacon.core.types.BLSPubkey;
+import org.ethereum.beacon.core.types.BLSSignature;
+import org.ethereum.beacon.core.types.EpochNumber;
+import org.ethereum.beacon.core.types.Gwei;
+import org.ethereum.beacon.crypto.Hashes;
+import org.ethereum.beacon.pow.DepositContract;
+import org.ethereum.beacon.ssz.Serializer;
+import org.ethereum.beacon.validator.BeaconChainAttester;
+import org.ethereum.beacon.validator.BeaconChainProposer;
+import org.ethereum.beacon.validator.BeaconChainValidator;
+import org.ethereum.beacon.validator.ValidatorService;
+import org.ethereum.beacon.validator.attester.BeaconChainAttesterImpl;
+import org.ethereum.beacon.validator.crypto.MessageSigner;
+import org.ethereum.beacon.validator.proposer.BeaconChainProposerImpl;
+import org.reactivestreams.Publisher;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import tech.pegasys.artemis.ethereum.core.Address;
+import tech.pegasys.artemis.ethereum.core.Hash32;
+import tech.pegasys.artemis.util.bytes.Bytes8;
+import tech.pegasys.artemis.util.bytes.BytesValue;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+
+import static org.ethereum.beacon.core.spec.SignatureDomains.DEPOSIT;
+
+public class ValidatorRegistrationServiceImpl implements ValidatorRegistrationService {
+  TransactionBuilder transactionBuilder;
+  TransactionGateway transactionGateway;
+  DepositContract depositContract;
+  Disposable depositSubscription = null;
+  ValidatorService validatorService = null;
+  SpecHelpers specHelpers;
+  Publisher<ObservableBeaconState> observablePublisher;
+  ObjectHasher<Hash32> sszHasher;
+  Serializer sszSerializer;
+  RegistrationStage currentStage = null;
+
+  // Validator
+  MessageSigner<BLSSignature> signer;
+  BLSPubkey pubKey;
+  Hash32 withdrawalCredentials = null;
+  Gwei amount = null;
+  Address eth1From = null;
+  BytesValue eth1PrivKey = null;
+  ValidatorRecord validatorRecord = null;
+
+  private ExecutorService executor;
+
+  public ValidatorRegistrationServiceImpl(
+      TransactionBuilder transactionBuilder,
+      TransactionGateway transactionGateway,
+      Publisher<ObservableBeaconState> observablePublisher,
+      SpecHelpers specHelpers) {
+    this.transactionBuilder = transactionBuilder;
+    this.transactionGateway = transactionGateway;
+    this.observablePublisher = observablePublisher;
+    this.specHelpers = specHelpers;
+    Function<BytesValue, Hash32> hashFunction = Hashes::keccak256;
+    sszHasher = SSZObjectHasher.create(hashFunction);
+    sszSerializer = Serializer.annotationSerializer();
+  }
+
+  @Override
+  public void start() {
+    this.executor =
+        Executors.newSingleThreadExecutor(
+            runnable -> {
+              Thread t = new Thread(runnable, "validator-registration-service");
+              t.setDaemon(true);
+              return t;
+            });
+    actionCircle();
+  }
+
+  private void actionCircle() {
+    RegistrationStage currentStage = getCurrentStage();
+    getCurrentStageFunction(currentStage).run();
+  }
+
+  private void changeCurrentStage(RegistrationStage newStage) {
+    this.currentStage = newStage;
+    // TODO: save to DB
+    actionCircle();
+  }
+
+  private RegistrationStage getCurrentStage() {
+    if (currentStage != null) {
+      return currentStage;
+    }
+
+    return RegistrationStage.SEND_TX; // TODO: read from DB
+  }
+
+  private Runnable getCurrentStageFunction(RegistrationStage stage) {
+    switch (stage) {
+      case SEND_TX:
+        {
+          return () -> executor.execute(this::sendDepositAndWait);
+        }
+      case AWAIT_INCLUSION:
+        {
+          return () -> executor.execute(this::awaitInclusion);
+        }
+      case AWAIT_ACTIVATION:
+        {
+          return () -> executor.execute(this::awaitActivation);
+        }
+      case VALIDATOR_START:
+        {
+          return () -> executor.execute(this::startValidator);
+        }
+      case COMPLETE:
+        {
+          return () -> executor.shutdown();
+        }
+      default:
+        {
+          throw new RuntimeException(
+              String.format("Service doesn't know how to handle registration stage %s", stage));
+        }
+    }
+  }
+
+  private void awaitActivation() {
+    Flux.from(observablePublisher)
+        .subscribe(
+            observableBeaconState -> {
+              BeaconState latestState = observableBeaconState.getLatestSlotState();
+              EpochNumber currentEpoch = specHelpers.get_current_epoch(latestState);
+              if (specHelpers.is_active_validator(validatorRecord, currentEpoch)) {
+                changeCurrentStage(RegistrationStage.VALIDATOR_START);
+              }
+            });
+  }
+
+  private void startValidator() {
+    if (validatorService == null) {
+      BlockTransition<BeaconStateEx> blockTransition = new PerBlockTransition(specHelpers);
+      StateTransition<BeaconStateEx> epochTransition = new PerEpochTransition(specHelpers);
+      BeaconChainProposer proposer =
+          new BeaconChainProposerImpl(
+              specHelpers,
+              specHelpers.getChainSpec(),
+              blockTransition,
+              epochTransition,
+              depositContract);
+      BeaconChainAttester attester =
+          new BeaconChainAttesterImpl(specHelpers, specHelpers.getChainSpec());
+      validatorService = new BeaconChainValidator(pubKey, proposer, attester, specHelpers, signer);
+      validatorService.start();
+      changeCurrentStage(RegistrationStage.COMPLETE);
+    }
+  }
+
+  private void awaitInclusion() {
+    if (depositSubscription == null) {
+      depositSubscription =
+          Flux.from(depositContract.getDepositStream())
+              .subscribe(
+                  deposit -> {
+                    onDeposit(deposit)
+                        .ifPresent(
+                            d -> {
+                              changeCurrentStage(RegistrationStage.AWAIT_ACTIVATION);
+                              try {
+                                BeaconState latestState =
+                                    Flux.from(observablePublisher)
+                                        .next()
+                                        .toFuture()
+                                        .get()
+                                        .getLatestSlotState();
+                                validatorRecord =
+                                    latestState.getValidatorRegistry().stream()
+                                        .filter(
+                                            record -> {
+                                              return record.getPubKey().equals(pubKey);
+                                            })
+                                        .findFirst()
+                                        .get();
+                              } catch (InterruptedException | ExecutionException e) {
+                                throw new RuntimeException(
+                                    "Cannot obtain latest state from Observable state publisher");
+                              }
+                              depositSubscription.dispose();
+                              depositSubscription = null;
+                            });
+                  });
+    }
+  }
+
+  private void sendDepositAndWait() {
+    submitDeposit(amount, eth1From, eth1PrivKey)
+        .thenRun(() -> changeCurrentStage(RegistrationStage.AWAIT_INCLUSION));
+  }
+
+  private Optional<Deposit> onDeposit(Deposit deposit) {
+    return Optional.of(deposit)
+        .filter(d -> d.getDepositData().getDepositInput().getPubKey().equals(pubKey));
+  }
+
+  private CompletableFuture<TransactionGateway.TxStatus> submitDeposit(
+      Gwei amount, Address eth1From, BytesValue eth1PrivKey) {
+    DepositInput depositInput = createDepositInput();
+    BytesValue tx = createTransaction(eth1From, eth1PrivKey, depositInput, amount);
+    return transactionGateway.send(tx);
+  }
+
+  private DepositInput createDepositInput() {
+    // To submit a deposit:
+    //
+    //    Pack the validator's initialization parameters into deposit_input, a DepositInput SSZ
+    // object.
+    //    Set deposit_input.proof_of_possession = EMPTY_SIGNATURE.
+    DepositInput preDepositInput =
+        new DepositInput(pubKey, withdrawalCredentials, BLSSignature.ZERO);
+    // Let proof_of_possession be the result of bls_sign of the hash_tree_root(deposit_input) with
+    // domain=DOMAIN_DEPOSIT.
+    Hash32 hash = sszHasher.getHash(preDepositInput);
+    BeaconState latestState = null;
+    try {
+      latestState = Flux.from(observablePublisher).next().toFuture().get().getLatestSlotState();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException("Cannot obtain latest state from Observable state publisher");
+    }
+    Bytes8 domain =
+        specHelpers.get_domain(
+            latestState.getForkData(), specHelpers.get_current_epoch(latestState), DEPOSIT);
+    BLSSignature signature = signer.sign(hash, domain);
+    // Set deposit_input.proof_of_possession = proof_of_possession.
+    DepositInput depositInput = new DepositInput(pubKey, withdrawalCredentials, signature);
+
+    return depositInput;
+  }
+
+  private BytesValue createTransaction(
+      Address eth1From, BytesValue eth1PrivKey, DepositInput depositInput, Gwei amount) {
+    // Let amount be the amount in Gwei to be deposited by the validator where MIN_DEPOSIT_AMOUNT <=
+    // amount <= MAX_DEPOSIT_AMOUNT.
+    if (amount.compareTo(specHelpers.getChainSpec().getMinDepositAmount()) < 0) {
+      throw new RuntimeException(
+          String.format(
+              "Deposit amount should be equal or greater than %s (defined by spec)",
+              specHelpers.getChainSpec().getMinDepositAmount()));
+    }
+    if (amount.compareTo(specHelpers.getChainSpec().getMaxDepositAmount()) > 0) {
+      throw new RuntimeException(
+          String.format(
+              "Deposit amount should be equal or less than %s (defined by spec)",
+              specHelpers.getChainSpec().getMaxDepositAmount()));
+    }
+    // Send a transaction on the Ethereum 1.0 chain to DEPOSIT_CONTRACT_ADDRESS executing deposit
+    // along with serialize(deposit_input) as the singular bytes input along with a deposit amount
+    // in Gwei.
+    BytesValue unsignedTx =
+        transactionBuilder.createTransaction(
+            eth1From.toString(), sszSerializer.encode2(depositInput), amount);
+
+    return transactionBuilder.signTransaction(unsignedTx, eth1PrivKey);
+  }
+}
