185f0fdc43a28c1df52572ffb8a023369dcb0fd7
Dmitrii Shmatko <leodex23@gmail.com>
2019-02-20 21:38:31 +0300

    start/cli: actualize configs and move plan to the top

--- emulator/config/src/main/java/org/ethereum/beacon/emulator/config/main/MainConfig.java
+++ emulator/config/src/main/java/org/ethereum/beacon/emulator/config/main/MainConfig.java
@@ -3,14 +3,11 @@ package org.ethereum.beacon.emulator.config.main;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.ethereum.beacon.emulator.config.Config;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-
 /** Main application configuration */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MainConfig implements Config {
-  private Configuration config;
   private Plan plan;
+  private Configuration config;
 
   public Configuration getConfig() {
     return config;
