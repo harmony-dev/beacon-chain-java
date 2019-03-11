2d2757fe081bb6616f7753468cccba3c6bde4c7f
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-03-11 15:11:18 +0600

    Polish plan CLI parameter description

--- start/cli/src/main/java/org/ethereum/beacon/command/RunSimulation.java
+++ start/cli/src/main/java/org/ethereum/beacon/command/RunSimulation.java
@@ -10,9 +10,9 @@ public class RunSimulation implements Runnable {
 
   @CommandLine.Parameters(
       index = "0",
-      paramLabel = "plan",
+      paramLabel = "plan.yml",
       description =
-          "A path to a simulation plan in YAML format\npass 'default' to run a simulation with default setup")
+          "A path to a file containing simulation plan in YAML format\nuse 'default' to run a simulation with default setup")
   private String plan;
 
   @CommandLine.Option(
