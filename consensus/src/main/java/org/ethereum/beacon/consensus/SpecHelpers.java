a127f527ac9ed33d676a53dc1986b0c9f0acb971
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-03-28 18:20:45 +0600

    Revert "Filter out attestations by shard number in get_winning_root_and_participants"
    This reverts commit bdfe260e3b646405a877b339f359db3e26bb0692.

--- consensus/src/main/java/org/ethereum/beacon/consensus/SpecHelpers.java
+++ consensus/src/main/java/org/ethereum/beacon/consensus/SpecHelpers.java
@@ -1773,7 +1773,6 @@ public interface SpecHelpers {
     all_attestations.addAll(state.getPreviousEpochAttestations().listCopy());
     List<PendingAttestation> valid_attestations =
         all_attestations.stream()
-            .filter(a -> a.getData().getShard().equals(shard))
             .filter(a -> a.getData()
                 .getPreviousCrosslink()
                 .equals(state.getLatestCrosslinks().get(shard)))
