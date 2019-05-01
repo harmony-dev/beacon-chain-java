46fc0ea5916412868fe65e0074a16ba962ef53e9
Dmitrii Shmatko <leodex23@gmail.com>
2019-05-01 18:16:45 +0300

    test: fix shuffling tests

--- test/src/test/java/org/ethereum/beacon/test/ShuffleTests.java
+++ test/src/test/java/org/ethereum/beacon/test/ShuffleTests.java
@@ -1,98 +1,78 @@
-//package org.ethereum.beacon.test;
-//
-//import org.ethereum.beacon.consensus.BeaconChainSpec;
-//import org.ethereum.beacon.core.spec.SpecConstants;
-//import org.ethereum.beacon.core.types.EpochNumber;
-//import org.ethereum.beacon.core.types.ShardNumber;
-//import org.ethereum.beacon.core.types.SlotNumber;
-//import org.ethereum.beacon.core.types.ValidatorIndex;
-//import org.ethereum.beacon.test.runner.shuffle.ShuffleRunner;
-//import org.ethereum.beacon.test.type.shuffle.ShuffleTest;
-//import org.junit.Test;
-//import tech.pegasys.artemis.ethereum.core.Hash32;
-//import tech.pegasys.artemis.util.collections.ReadList;
-//import tech.pegasys.artemis.util.uint.UInt64;
-//
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//
-//public class ShuffleTests extends TestUtils {
-//  private String TESTS_DIR = "shuffling";
-//  private BeaconChainSpec spec;
-//
-//  public ShuffleTests() {
-//    // xxx EPOCH_LENGTH = 2**6  # 64 slots, 6.4 minutes
-//    // xxx FAR_FUTURE_EPOCH = 2**64 - 1  # uint64 max
-//    // xxx SHARD_COUNT = 2**10  # 1024
-//    // xxx TARGET_COMMITTEE_SIZE = 2**7  # 128 validators
-//    // xxx ENTRY_EXIT_DELAY = 2**2 # 4 epochs
-//    SpecConstants specConstants =
-//        new SpecConstants() {
-//          @Override
-//          public EpochNumber getFarFutureEpoch() {
-//            return EpochNumber.castFrom(UInt64.MAX_VALUE);
-//          }
-//
-//          @Override
-//          public ValidatorIndex getTargetCommitteeSize() {
-//            return ValidatorIndex.of(128);
-//          }
-//
-//          @Override
-//          public SlotNumber.EpochLength getSlotsPerEpoch() {
-//            return new SlotNumber.EpochLength(UInt64.valueOf(1 << 6));
-//          }
-//
-//          @Override
-//          public ShardNumber getShardCount() {
-//            return ShardNumber.of(1024);
-//          }
-//
-//          @Override
-//          public EpochNumber getActivationExitDelay() {
-//            return EpochNumber.of(4);
-//          }
-//        };
-//    this.spec = BeaconChainSpec.createWithDefaultHasher(specConstants);
-//  }
-//
-//  @Test
-//  public void testShuffling() {
-//    Path sszTestsPath = Paths.get(PATH_TO_TESTS, TESTS_DIR);
-//    runTestsInResourceDir(
-//        sszTestsPath,
-//        ShuffleTest.class,
-//        testCase -> {
-//          ShuffleRunner testCaseRunner =
-//              new ShuffleRunner(
-//                  testCase,
-//                  spec,
-//                  objects ->
-//                      spec.get_shuffling(
-//                          objects.getValue0(), objects.getValue1(), objects.getValue2()));
-//          return testCaseRunner.run();
-//        });
-//  }
-//
-//  /**
-//   * Runs tests on optimized version of get_shuffling, {@link BeaconChainSpec#get_shuffling2(Hash32,
-//   * ReadList, EpochNumber)}
-//   */
-//  @Test
-//  public void testShuffling2() {
-//    Path sszTestsPath = Paths.get(PATH_TO_TESTS, TESTS_DIR);
-//    runTestsInResourceDir(
-//        sszTestsPath,
-//        ShuffleTest.class,
-//        testCase -> {
-//          ShuffleRunner testCaseRunner =
-//              new ShuffleRunner(
-//                  testCase,
-//                  spec,
-//                  objects ->
-//                      spec.get_shuffling2(
-//                          objects.getValue0(), objects.getValue1(), objects.getValue2()));
-//          return testCaseRunner.run();
-//        });
-//  }
-//}
+package org.ethereum.beacon.test;
+
+import org.ethereum.beacon.consensus.BeaconChainSpec;
+import org.ethereum.beacon.core.types.ValidatorIndex;
+import org.ethereum.beacon.test.runner.shuffle.ShuffleRunner;
+import org.ethereum.beacon.test.type.shuffle.ShuffleTest;
+import org.junit.Test;
+import tech.pegasys.artemis.ethereum.core.Hash32;
+import tech.pegasys.artemis.util.uint.UInt64;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class ShuffleTests extends TestUtils {
+  private String TESTS_DIR = "shuffling";
+  private String TESTS_SUBDIR = "core";
+
+  @Test
+  public void testShuffling() {
+    Path testFileDir = Paths.get(PATH_TO_TESTS, TESTS_DIR, TESTS_SUBDIR);
+    runTestsInResourceDir(
+        testFileDir,
+        ShuffleTest.class,
+        input -> {
+          ShuffleRunner testRunner =
+              new ShuffleRunner(
+                  input.getValue0(),
+                  input.getValue1(),
+                  objects ->
+                      IntStream.range(0, objects.getValue2())
+                          .mapToObj(
+                              i -> {
+                                UInt64 permuted_index =
+                                    input
+                                        .getValue1()
+                                        .get_permuted_index(
+                                            UInt64.valueOf(i),
+                                            UInt64.valueOf(objects.getValue2()),
+                                            objects.getValue1());
+                                return objects.getValue0().get(permuted_index.getIntValue());
+                              })
+                          .collect(toList()));
+          return testRunner.run();
+        });
+  }
+
+  /**
+   * Runs tests on optimized version of get_shuffling, like in {@link
+   * BeaconChainSpec#compute_committee2(List, Hash32, int, int)}
+   */
+  @Test
+  public void testShuffling2() {
+    Path testFileDir = Paths.get(PATH_TO_TESTS, TESTS_DIR, TESTS_SUBDIR);
+    runTestsInResourceDir(
+        testFileDir,
+        ShuffleTest.class,
+        input -> {
+          ShuffleRunner testRunner =
+              new ShuffleRunner(
+                  input.getValue0(),
+                  input.getValue1(),
+                  objects -> {
+                    List<UInt64> permuted_indices =
+                        input
+                            .getValue1()
+                            .get_permuted_list(objects.getValue0(), objects.getValue1());
+                    return permuted_indices.subList(0, objects.getValue2()).stream()
+                        .map(ValidatorIndex::new)
+                        .collect(toList());
+                  });
+          return testRunner.run();
+        });
+  }
+}
