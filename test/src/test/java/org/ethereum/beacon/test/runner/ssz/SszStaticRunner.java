98bca29bce21efa5eb5284472c9b79cd5e2ca1cb
Dmitrii Shmatko <leodex23@gmail.com>
2019-05-01 16:43:44 +0300

    test: fix code style of SszStaticRunner

--- test/src/test/java/org/ethereum/beacon/test/runner/ssz/SszStaticRunner.java
+++ test/src/test/java/org/ethereum/beacon/test/runner/ssz/SszStaticRunner.java
@@ -6,18 +6,14 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
 import org.ethereum.beacon.consensus.BeaconChainSpec;
-import org.ethereum.beacon.consensus.hasher.SSZObjectHasher;
 import org.ethereum.beacon.core.spec.SpecConstantsResolver;
-import org.ethereum.beacon.crypto.Hashes;
 import org.ethereum.beacon.ssz.SSZBuilder;
-import org.ethereum.beacon.ssz.SSZHasher;
 import org.ethereum.beacon.ssz.SSZSerializer;
 import org.ethereum.beacon.ssz.creator.ConstructorObjCreator;
 import org.ethereum.beacon.test.runner.Runner;
 import org.ethereum.beacon.test.runner.ssz.mapper.ObjectSerializer;
 import org.ethereum.beacon.test.type.TestCase;
 import org.ethereum.beacon.test.type.ssz.SszStaticCase;
-import tech.pegasys.artemis.ethereum.core.Hash32;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 
 import java.io.IOException;
@@ -26,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.ethereum.beacon.test.SilentAsserts.assertEquals;
@@ -35,7 +30,8 @@ import static org.ethereum.beacon.test.SilentAsserts.assertHexStrings;
 /**
  * SSZ, Hash root, signing root tests for known types
  *
- * <p>Test format description: <a href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/test_formats/ssz_static/core.md">https://github.com/ethereum/eth2.0-specs/blob/dev/specs/test_formats/ssz_static/core.md</a>
+ * <p>Test format description: <a
+ * href="https://github.com/ethereum/eth2.0-specs/blob/dev/specs/test_formats/ssz_static/core.md">https://github.com/ethereum/eth2.0-specs/blob/dev/specs/test_formats/ssz_static/core.md</a>
  */
 public class SszStaticRunner implements Runner {
   private SszStaticCase testCase;
@@ -46,32 +42,34 @@ public class SszStaticRunner implements Runner {
 
   public SszStaticRunner(TestCase testCase, BeaconChainSpec spec) {
     if (!(testCase instanceof SszStaticCase)) {
-      throw new RuntimeException(
-          "TestCase runner accepts only SszStaticCase.class as input!");
+      throw new RuntimeException("TestCase runner accepts only SszStaticCase.class as input!");
     }
     this.testCase = (SszStaticCase) testCase;
     SSZBuilder builder = new SSZBuilder();
-    this.sszSerializer = builder
-        .withExternalVarResolver(new SpecConstantsResolver(spec.getConstants()))
-        .buildSerializer();
+    this.sszSerializer =
+        builder
+            .withExternalVarResolver(new SpecConstantsResolver(spec.getConstants()))
+            .buildSerializer();
     this.yamlMapper = new ObjectMapper(new YAMLFactory());
     Set<ClassInfo> mappers = findClassesInPackage("org.ethereum.beacon.test.runner.ssz.mapper");
-    mappers.stream().map(this::fromInfo).map((Class mapperType) -> createInstance(mapperType, yamlMapper)).forEach(m -> {
-      if (m == null) {
-        return; // Couldn't create instance, for example, for interface
-      }
-      ObjectSerializer mapper = (ObjectSerializer) m;
-      objectSerializers.put(mapper.accepts(), mapper);
-    });
+    mappers.stream()
+        .map(this::fromInfo)
+        .map((Class mapperType) -> createInstance(mapperType, yamlMapper))
+        .forEach(
+            m -> {
+              if (m == null) {
+                return; // Couldn't create instance, for example, for interface
+              }
+              ObjectSerializer mapper = (ObjectSerializer) m;
+              objectSerializers.put(mapper.accepts(), mapper);
+            });
     this.spec = spec;
   }
 
-  private ObjectSerializer createInstance(Class<? extends ObjectSerializer> mapperType, ObjectMapper jacksonMapper) {
+  private ObjectSerializer createInstance(
+      Class<? extends ObjectSerializer> mapperType, ObjectMapper jacksonMapper) {
     return ConstructorObjCreator.createInstanceWithConstructor(
-        mapperType,
-        new Class[] {ObjectMapper.class},
-        new Object[] {jacksonMapper}
-    );
+        mapperType, new Class[] {ObjectMapper.class}, new Object[] {jacksonMapper});
   }
 
   private Set<ClassInfo> findClassesInPackage(String packageName) {
@@ -101,21 +99,28 @@ public class SszStaticRunner implements Runner {
     Set<ClassInfo> coreTypes = findClassesInPackage("org.ethereum.beacon.core");
     final String searchName;
     if (testCase.getTypeName().equals("Validator")) {
-      searchName = "ValidatorRecord";  // XXX: we have different naming
-    } else if (testCase.getTypeName().equals("BeaconState"))  {
-      searchName = "BeaconStateImpl";  // XXX: we have several implementations
+      searchName = "ValidatorRecord"; // XXX: we have different naming
+    } else if (testCase.getTypeName().equals("BeaconState")) {
+      searchName = "BeaconStateImpl"; // XXX: we have several implementations
     } else {
       searchName = testCase.getTypeName();
     }
-    List<ClassInfo> classes = coreTypes.stream().filter((type) -> type.getSimpleName().equals(searchName)).collect(Collectors.toList());
+    List<ClassInfo> classes =
+        coreTypes.stream()
+            .filter((type) -> type.getSimpleName().equals(searchName))
+            .collect(Collectors.toList());
     ClassInfo valueTypeInfo;
     if (classes.size() != 1) {
-      return Optional.of(String.format("Failed: should be only one appropriate core class for %s (%s classes found) ", testCase.getTypeName(), classes.size()));
+      return Optional.of(
+          String.format(
+              "Failed: should be only one appropriate core class for %s (%s classes found) ",
+              testCase.getTypeName(), classes.size()));
     } else {
       valueTypeInfo = classes.get(0);
     }
     Class valueType = fromInfo(valueTypeInfo);
-    Object fromSerialized = sszSerializer.decode(BytesValue.fromHexString(testCase.getSerialized()), valueType);
+    Object fromSerialized =
+        sszSerializer.decode(BytesValue.fromHexString(testCase.getSerialized()), valueType);
     Object simplified = objectSerializers.get(valueType).map(fromSerialized);
     ObjectNode expectedValue = yamlMapper.convertValue(testCase.getValue(), ObjectNode.class);
     // XXX: expected goes second as our constructed node contains with overridden `equals` operators
