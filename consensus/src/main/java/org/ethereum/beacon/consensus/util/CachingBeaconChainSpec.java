5719d5018186787bd0c338d7e408b0b99d090559
Mikhail Kalinin <noblesse.knight@gmail.com>
2019-05-16 15:49:22 +0600

    Improve beacon chain spec caches

--- consensus/src/main/java/org/ethereum/beacon/consensus/util/CachingBeaconChainSpec.java
+++ consensus/src/main/java/org/ethereum/beacon/consensus/util/CachingBeaconChainSpec.java
@@ -7,14 +7,18 @@ import java.util.function.Function;
 import org.ethereum.beacon.consensus.BeaconChainSpecImpl;
 import org.ethereum.beacon.consensus.hasher.ObjectHasher;
 import org.ethereum.beacon.core.BeaconState;
+import org.ethereum.beacon.core.operations.attestation.AttestationData;
 import org.ethereum.beacon.core.spec.SpecConstants;
 import org.ethereum.beacon.core.types.BLSPubkey;
+import org.ethereum.beacon.core.types.Bitfield;
 import org.ethereum.beacon.core.types.EpochNumber;
+import org.ethereum.beacon.core.types.Gwei;
 import org.ethereum.beacon.core.types.ShardNumber;
 import org.ethereum.beacon.core.types.ValidatorIndex;
 import org.ethereum.beacon.util.cache.Cache;
 import org.ethereum.beacon.util.cache.CacheFactory;
 import org.javatuples.Pair;
+import org.javatuples.Triplet;
 import tech.pegasys.artemis.ethereum.core.Hash32;
 import tech.pegasys.artemis.util.bytes.Bytes32;
 import tech.pegasys.artemis.util.bytes.BytesValue;
@@ -25,8 +29,10 @@ public class CachingBeaconChainSpec extends BeaconChainSpecImpl {
   private final Cache<Pair<List<? extends UInt64>, Bytes32>, List<UInt64>> shufflerCache;
   private final Cache<Object, Hash32> hashTreeRootCache;
   private final Cache<Object, Hash32> signedRootCache;
-  private final Cache<Pair<EpochNumber, ShardNumber>, List<ValidatorIndex>> crosslinkCommitteesCache;
+  private final Cache<Triplet<Hash32, EpochNumber, ShardNumber>, List<ValidatorIndex>> crosslinkCommitteesCache;
   private final Cache<EpochNumber, List<ValidatorIndex>> activeValidatorsCache;
+  private final Cache<EpochNumber, Gwei> totalActiveBalanceCache;
+  private final Cache<Triplet<Hash32, AttestationData, Bitfield>, List<ValidatorIndex>> attestingIndicesCache;
 
   private ValidatorIndex maxCachedIndex = ValidatorIndex.ZERO;
   private final Map<BLSPubkey, ValidatorIndex> pubkeyToIndexCache = new ConcurrentHashMap<>();
@@ -49,6 +55,8 @@ public class CachingBeaconChainSpec extends BeaconChainSpecImpl {
     this.signedRootCache = factory.createLRUCache(1024);
     this.crosslinkCommitteesCache = factory.createLRUCache(128);
     this.activeValidatorsCache = factory.createLRUCache(32);
+    this.totalActiveBalanceCache = factory.createLRUCache(32);
+    this.attestingIndicesCache = factory.createLRUCache(1024);
   }
 
   public CachingBeaconChainSpec(
@@ -95,7 +103,7 @@ public class CachingBeaconChainSpec extends BeaconChainSpecImpl {
 
   @Override
   public List<ValidatorIndex> get_crosslink_committee(BeaconState state, EpochNumber epoch, ShardNumber shard) {
-    return crosslinkCommitteesCache.get(Pair.with(epoch, shard),
+    return crosslinkCommitteesCache.get(Triplet.with(hash(state), epoch, shard),
         s -> super.get_crosslink_committee(state, epoch, shard));
   }
 
@@ -104,7 +112,28 @@ public class CachingBeaconChainSpec extends BeaconChainSpecImpl {
     return activeValidatorsCache.get(epoch, e -> super.get_active_validator_indices(state, epoch));
   }
 
+  @Override
+  public Gwei get_total_active_balance(BeaconState state) {
+    return totalActiveBalanceCache.get(
+        get_current_epoch(state), e -> super.get_total_active_balance(state));
+  }
+
+  @Override
+  public List<ValidatorIndex> get_attesting_indices(
+      BeaconState state, AttestationData attestation_data, Bitfield bitfield) {
+    return attestingIndicesCache.get(
+        Triplet.with(hash(state), attestation_data, bitfield),
+        e -> super.get_attesting_indices(state, attestation_data, bitfield));
+  }
+
   public boolean isCacheEnabled() {
     return cacheEnabled;
+  }
+
+  /**
+   * Do not use {@link #hash_tree_root(Object)} directly as it causes false counts in benchmarks.
+   */
+  private Hash32 hash(Object object) {
+    return hashTreeRootCache.get(object, (o) -> getObjectHasher().getHash(o));
   }
 }
