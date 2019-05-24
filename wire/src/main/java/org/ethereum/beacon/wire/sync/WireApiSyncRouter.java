59a0b2328981e6fb44f629becbd5608ec8dea244
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-05-24 16:23:48 +0300

    Modify zip() operator prefetch parameter to 1. By default it is 32 and this cause tasks submitted to already disconnected api

--- wire/src/main/java/org/ethereum/beacon/wire/sync/WireApiSyncRouter.java
+++ wire/src/main/java/org/ethereum/beacon/wire/sync/WireApiSyncRouter.java
@@ -1,8 +1,11 @@
 package org.ethereum.beacon.wire.sync;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ethereum.beacon.stream.RxUtil;
 import org.ethereum.beacon.util.Utils;
 import org.ethereum.beacon.wire.Feedback;
@@ -24,28 +27,34 @@ import reactor.core.publisher.ReplayProcessor;
  * When no single delegate api available all calls are queued until any api arises
  */
 public class WireApiSyncRouter implements WireApiSync {
+  private static final Logger logger = LogManager.getLogger(WireApiSyncRouter.class);
 
   private final ReplayProcessor<Consumer<WireApiSync>> tasks = ReplayProcessor.create(64);
   private final FluxSink<Consumer<WireApiSync>> tasksSink = tasks.sink();
+  private final AtomicInteger pendingTasks = new AtomicInteger();
 
   public WireApiSyncRouter(
       Publisher<WireApiSync> addedPeersStream,
       Publisher<WireApiSync> removedPeersStream) {
 
-    Publisher<WireApiSync> freePeersStream =
+    Flux<WireApiSync> freePeersStream =
         RxUtil.collect(addedPeersStream, removedPeersStream)
+            .doOnNext(activePeers -> logger.info("Active APIs count: " + activePeers.size()))
             .switchMap(
                 activePeers ->
                     activePeers.isEmpty() ? Flux.never() : Flux.fromIterable(activePeers).repeat(),
                 1);
 
-    Flux.zip(freePeersStream, tasks)
-      .subscribe(p -> p.getT2().accept(p.getT1()));
+    freePeersStream.zipWith(tasks, 1)
+        .doOnNext(p -> pendingTasks.decrementAndGet())
+        .subscribe(p -> p.getT2().accept(p.getT1()));
   }
 
   private <C> CompletableFuture<C> submitAsyncTask(Function<WireApiSync, CompletableFuture<C>> task) {
     CompletableFuture<C> ret = new CompletableFuture<>();
     tasksSink.next(api -> Utils.futureForward(task.apply(api), ret));
+    int cnt = pendingTasks.incrementAndGet();
+    logger.debug("New task submitted. Pending tasks: " + cnt);
     return ret;
   }
 
