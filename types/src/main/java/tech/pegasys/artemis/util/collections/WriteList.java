9fe28be8673b903eb3db4f68b9a38fdad515e37e
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-06-05 11:22:49 +0300

    Remove obsolete import

--- types/src/main/java/tech/pegasys/artemis/util/collections/WriteList.java
+++ types/src/main/java/tech/pegasys/artemis/util/collections/WriteList.java
@@ -1,12 +1,10 @@
 package tech.pegasys.artemis.util.collections;
 
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import org.jetbrains.annotations.NotNull;
-import tech.pegasys.artemis.util.uint.UInt24;
 
 public interface WriteList<IndexType extends Number, ValueType>
     extends WriteVector<IndexType, ValueType> {
