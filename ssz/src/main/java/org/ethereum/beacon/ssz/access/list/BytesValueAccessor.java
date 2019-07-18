d5bf4f3b3f5509f98e70ca1828908737bece31be
Dmitrii Shmatko <leodex23@gmail.com>
2019-07-18 16:05:35 +0300

    ssz: fix BytesXX vector byteValues deserialization

--- ssz/src/main/java/org/ethereum/beacon/ssz/access/list/BytesValueAccessor.java
+++ ssz/src/main/java/org/ethereum/beacon/ssz/access/list/BytesValueAccessor.java
@@ -2,7 +2,13 @@ package org.ethereum.beacon.ssz.access.list;
 
 import java.util.List;
 import org.ethereum.beacon.ssz.access.SSZField;
+import org.ethereum.beacon.ssz.creator.ConstructorObjCreator;
 import org.ethereum.beacon.ssz.type.SSZType;
+import tech.pegasys.artemis.util.bytes.Bytes1;
+import tech.pegasys.artemis.util.bytes.Bytes32;
+import tech.pegasys.artemis.util.bytes.Bytes4;
+import tech.pegasys.artemis.util.bytes.Bytes48;
+import tech.pegasys.artemis.util.bytes.Bytes96;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 
 public class BytesValueAccessor extends AbstractListAccessor {
@@ -31,7 +37,21 @@ public class BytesValueAccessor extends AbstractListAccessor {
         for (int i = 0; i < children.size(); i++) {
           vals[i] = ((Number) children.get(i)).byteValue();
         }
-        return BytesValue.wrap(vals);
+        BytesValue value = BytesValue.wrap(vals);
+        // TODO: refactor me pleasse!
+        if (sszType.getSize() == 1) {
+          return Bytes1.wrap(value.extractArray());
+        } else if (sszType.getSize() == 4) {
+          return Bytes4.wrap(value.extractArray());
+        } else if (sszType.getSize() == 32) {
+          return Bytes32.wrap(value.extractArray());
+        } else if (sszType.getSize() == 48) {
+          return Bytes48.wrap(value.extractArray());
+        } else if (sszType.getSize() == 96) {
+          return Bytes96.wrap(value.extractArray());
+        }
+
+        return value;
       }
     };
   }
