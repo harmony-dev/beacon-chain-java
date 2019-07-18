0c3e0390e0cc0b900b8b61d1441adacd7bfab466
Dmitrii Shmatko <leodex23@gmail.com>
2019-07-18 16:02:05 +0300

    test: add mappers for Checkpoint and CompactCommittee

---
+++ test/src/test/java/org/ethereum/beacon/test/runner/ssz/mapper/CheckpointSerializer.java
@@ -0,0 +1,25 @@
+package org.ethereum.beacon.test.runner.ssz.mapper;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.ethereum.beacon.core.state.Checkpoint;
+
+public class CheckpointSerializer implements ObjectSerializer<Checkpoint> {
+  private com.fasterxml.jackson.databind.ObjectMapper mapper;
+
+  public CheckpointSerializer(com.fasterxml.jackson.databind.ObjectMapper mapper) {
+    this.mapper = mapper;
+  }
+
+  @Override
+  public Class accepts() {
+    return Checkpoint.class;
+  }
+
+  @Override
+  public ObjectNode map(Checkpoint instance) {
+    ObjectNode checkpoint = mapper.createObjectNode();
+    checkpoint.put("epoch", instance.getEpoch().toString());
+    checkpoint.put("root", instance.getRoot().toString());
+    return checkpoint;
+  }
+}
