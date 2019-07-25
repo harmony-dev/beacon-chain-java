bd3033f973ceb8ea232748100db2953cfb9478e5
Dmitrii Shmatko <leodex23@gmail.com>
2019-07-25 20:35:16 +0300

    disable memory heavy test (sanity/blocks/mainnet)

--- test/src/test/java/org/ethereum/beacon/test/StateSanityTests.java
+++ test/src/test/java/org/ethereum/beacon/test/StateSanityTests.java
@@ -2,8 +2,10 @@ package org.ethereum.beacon.test;
 
 import org.ethereum.beacon.test.runner.state.StateRunner;
 import org.ethereum.beacon.test.type.state.StateTest;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -25,15 +27,33 @@ public class StateSanityTests extends TestUtils {
   }
 
   @Test
-  public void testSanityBlocks() {
+  public void testSanityBlocksMin() {
     final String type = "blocks";
-    Path testFileDir = Paths.get(PATH_TO_TESTS, SUBDIR, type);
-    runTestsInResourceDir(
-        testFileDir,
-        StateTest.class,
+    File testFile =
+        getResourceFile(
+            Paths.get(PATH_TO_TESTS, SUBDIR, type, "sanity_blocks_minimal.yaml").toString());
+    runAllTestsInFile(
+        testFile,
         input -> {
           StateRunner testRunner = new StateRunner(input.getValue0(), input.getValue1(), type);
           return testRunner.run();
-        });
+        },
+        StateTest.class);
+  }
+
+  @Test
+  @Ignore("Requires a lot of memory")
+  public void testSanityBlocksMain() {
+    final String type = "blocks";
+    File testFile =
+        getResourceFile(
+            (Paths.get(PATH_TO_TESTS, SUBDIR, type, "sanity_blocks_mainnet.yaml").toString()));
+    runAllTestsInFile(
+        testFile,
+        input -> {
+          StateRunner testRunner = new StateRunner(input.getValue0(), input.getValue1(), type);
+          return testRunner.run();
+        },
+        StateTest.class);
   }
 }
