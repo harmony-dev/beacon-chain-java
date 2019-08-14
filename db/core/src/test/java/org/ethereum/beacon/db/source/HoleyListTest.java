9384595647a3a66b71bbf1a84f661116fa8ebb0a
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-08-14 16:40:13 +0300

    HoleyListTest

---
+++ db/core/src/test/java/org/ethereum/beacon/db/source/HoleyListTest.java
@@ -0,0 +1,79 @@
+package org.ethereum.beacon.db.source;
+
+import org.junit.*;
+
+import java.util.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class HoleyListTest {
+
+    private final Long TEST_KEY_0 = 0L;
+    private final Long TEST_KEY_1 = 1L;
+    private final Long TEST_KEY_LESS_ZERO = -1L;
+    private final String TEST_VALUE = "test_value";
+    private final String TEST_DEFAULT_VALUE = "test_default_value";
+
+    private HoleyList<String> list;
+
+    @Before
+    public void setUp() {
+        list = new HoleyList<String>() {
+            private final Map<Long, String> store = new HashMap<>();
+
+            public long size() {
+                return store.size();
+            }
+
+            public void put(long idx, String value) {
+                store.put(idx, value);
+            }
+
+            public Optional<String> get(long idx) {
+                return Optional.of(store.get(idx));
+            }
+        };
+
+        assertThat(list).isNotNull();
+    }
+
+    @Test
+    public void testAdd() {
+        list.put(TEST_KEY_0, TEST_VALUE);
+        final long indexToAdd = list.size();
+        list.add(TEST_VALUE);
+
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(indexToAdd)).isPresent().hasValue(TEST_VALUE);
+    }
+
+    @Test
+    public void testAddNull() {
+        list.put(TEST_KEY_0, TEST_VALUE);
+        final long indexToAdd = list.size();
+        list.add(null);
+
+        assertThat(list.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testUpdateExistingValue() {
+        list.put(TEST_KEY_0, TEST_VALUE);
+        assertThat(list.update(TEST_KEY_0, key -> "")).isPresent().hasValue("");
+    }
+
+    @Test
+    public void testUpdateExistingValueOverIndex() {
+        list.put(TEST_KEY_0, TEST_VALUE);
+        assertThat(list.update(TEST_KEY_1, key -> "")).isNotPresent();
+        assertThat(list.update(TEST_KEY_LESS_ZERO, key -> "")).isNotPresent();
+    }
+
+    @Test
+    public void testUpdateExistingValueOrPutDefault() {
+        list.put(TEST_KEY_0, TEST_VALUE);
+        assertThat(list.update(TEST_KEY_0, key -> "", () -> TEST_DEFAULT_VALUE)).isEqualTo("");
+        assertThat(list.update(TEST_KEY_1, key -> "", () -> TEST_DEFAULT_VALUE)).isEqualTo(TEST_DEFAULT_VALUE);
+    }
+
+}
--- db/core/src/test/java/org/ethereum/beacon/db/source/LinkedDataSourceTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/source/LinkedDataSourceTest.java
@@ -1,21 +1,81 @@
 package org.ethereum.beacon.db.source;
 
 import org.ethereum.beacon.db.source.impl.HashMapDataSource;
-import org.junit.Test;
+import org.junit.*;
 
-import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
+import javax.annotation.Nonnull;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class LinkedDataSourceTest {
 
+    private LinkedDataSource<String, String, String, String> dataSource;
+
+    @Before
+    public void setUp() {
+        dataSource = new LinkedDataSource<String, String, String, String>() {
+            public Optional<String> get(@Nonnull String key) {
+                return Optional.empty();
+            }
+
+            public void put(@Nonnull String key, @Nonnull String value) {
+
+            }
+
+            public void remove(@Nonnull String key) {
+
+            }
+
+            public void flush() {
+
+            }
+
+            @Nonnull
+            public DataSource<String, String> getUpstream() {
+                return null;
+            }
+        };
+
+        assertThat(dataSource).isNotNull();
+    }
+
     @Test
     public void testSetUpstream() {
-        final LinkedDataSource<String, String, String, String> dataSource = new WriteCacheImpl<>(new HashMapDataSource<>());
         assertThatThrownBy(() -> dataSource.setUpstream(new HashMapDataSource<>())).isInstanceOf(UnsupportedOperationException.class);
     }
 
     @Test
-    public void testValidSourceCreation() {
-        assertThatThrownBy(() -> new WriteCacheImpl<String, String>(null)).isInstanceOf(NullPointerException.class);
+    public void testGetNullUpstream() {
+        assertThat(dataSource.getUpstream()).isNull();
     }
 
+    @Test
+    public void testGetUpstream() {
+        dataSource = new LinkedDataSource<String, String, String, String>() {
+            public Optional<String> get(@Nonnull String key) {
+                return Optional.empty();
+            }
+
+            public void put(@Nonnull String key, @Nonnull String value) {
+
+            }
+
+            public void remove(@Nonnull String key) {
+
+            }
+
+            public void flush() {
+
+            }
+
+            @Nonnull
+            public DataSource<String, String> getUpstream() {
+                return new HashMapDataSource<>();
+            }
+        };
+        assertThat(dataSource).isNotNull();
+        assertThat(dataSource.getUpstream()).isNotNull();
+    }
 }
