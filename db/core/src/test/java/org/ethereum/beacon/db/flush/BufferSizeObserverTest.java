452ffa1bb8666df7d7ec3765f46f3e9253327bbf
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-09-05 12:32:53 +0300

    test: add param tests to BufferSizeObserverTest

--- db/core/src/test/java/org/ethereum/beacon/db/flush/BufferSizeObserverTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/flush/BufferSizeObserverTest.java
@@ -3,6 +3,11 @@ package org.ethereum.beacon.db.flush;
 import org.ethereum.beacon.db.source.WriteBuffer;
 import org.ethereum.beacon.db.source.impl.HashMapDataSource;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -11,8 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BufferSizeObserverTest {
 
-    private final String TEST_KEY = "TEST_KEY";
-    private final String TEST_VALUE = "TEST_VALUE";
     private WriteBuffer<String, String> buffer;
     private WriteBuffer<String, String> commitTrack;
     private BufferSizeObserver observer;
@@ -28,20 +31,39 @@ class BufferSizeObserverTest {
         assertThat(observer).isNotNull();
     }
 
-    @Test
-    void testBufferSizeObserverCreation() {
-        assertThat(new BufferSizeObserver(null, null, Long.MIN_VALUE)).isNotNull();
-        assertThat(new BufferSizeObserver(null, new WriteBuffer(new HashMapDataSource(), true), Long.MIN_VALUE)).isNotNull();
-        assertThat(new BufferSizeObserver(new WriteBuffer(new HashMapDataSource(), true), null, Long.MIN_VALUE)).isNotNull();
-
-        assertThat(new BufferSizeObserver(null, null, Long.MAX_VALUE)).isNotNull();
-        assertThat(new BufferSizeObserver(null, new WriteBuffer(new HashMapDataSource(), true), Long.MAX_VALUE)).isNotNull();
-        assertThat(new BufferSizeObserver(new WriteBuffer(new HashMapDataSource(), true), null, Long.MAX_VALUE)).isNotNull();
+    @ParameterizedTest
+    @MethodSource("nullArgumentsProvider")
+    void testBufferSizeObserverCreation(WriteBuffer<String, String> buffer, WriteBuffer<String, String> commitTrack, long bufferLimitSize) {
+        assertThat(new BufferSizeObserver(buffer, commitTrack, bufferLimitSize)).isNotNull();
     }
 
+    private static Stream<Arguments> nullArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(null, null, Long.MIN_VALUE),
+                Arguments.of(null, new WriteBuffer(new HashMapDataSource(), true), Long.MIN_VALUE),
+                Arguments.of(new WriteBuffer(new HashMapDataSource(), true), null, Long.MIN_VALUE),
+                Arguments.of(null, null, Long.MAX_VALUE),
+                Arguments.of(null, new WriteBuffer(new HashMapDataSource(), true), Long.MAX_VALUE),
+                Arguments.of(new WriteBuffer(new HashMapDataSource(), true), null, Long.MAX_VALUE)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("nullKeyValueArgumentsProvider")
+    void testPutNullKeyValue(String key, String value) {
+        assertThatThrownBy(() -> buffer.put(key, value)).isInstanceOf(NullPointerException.class);
+    }
+
+    private static Stream<Arguments> nullKeyValueArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(null, null),
+                Arguments.of("not_null", null)
+        );
+    }
 
     @Test
     void testCreate() {
+
         String TEST_KEY_0 = "TEST_KEY_0";
         String TEST_VALUE_0 = "TEST_VALUE_0";
         String TEST_KEY_1 = "TEST_KEY_1";
@@ -63,8 +85,6 @@ class BufferSizeObserverTest {
         buffer.put(TEST_KEY_3, TEST_VALUE_3);
         buffer.put(TEST_KEY_4, TEST_VALUE_4);
 
-        assertThatThrownBy(() -> buffer.put(null, null)).isInstanceOf(NullPointerException.class);
-        assertThatThrownBy(() -> buffer.put("NOT_NULL", null)).isInstanceOf(NullPointerException.class);
 
         assertThat(buffer.get(TEST_KEY_0)).isPresent();
         assertThat(buffer.getCacheEntry(TEST_KEY_0)).isPresent();
@@ -80,42 +100,44 @@ class BufferSizeObserverTest {
         assertThat(buffer.getCacheEntry(TEST_KEY_5)).isPresent();
 
         observer.commit();
-        assertThat(buffer.getUpstream().get(TEST_KEY_5)).isPresent();
-        assertThat(buffer.getUpstream().get(TEST_KEY_5)).hasValue(TEST_VALUE_5);
-
+        assertThat(buffer.getUpstream().get(TEST_KEY_5)).isPresent().hasValue(TEST_VALUE_5);
     }
 
-    @Test
-    void testFlush() {
-        buffer.put(TEST_KEY, TEST_VALUE);
-        assertThat(buffer.get(TEST_KEY)).isPresent();
-        assertThat(buffer.get(TEST_KEY)).hasValue(TEST_VALUE);
+    private static Stream<Arguments> keyValueArgumentsProvider() {
+        return Stream.of(
+                Arguments.of("test_key", "test_value")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("keyValueArgumentsProvider")
+    void testFlush(String key, String value) {
+        buffer.put(key, value);
+        assertThat(buffer.get(key)).isPresent().hasValue(value);
 
         observer.flush();
-        assertThat(buffer.getUpstream().get(TEST_KEY)).isPresent();
-        assertThat(buffer.getUpstream().get(TEST_KEY)).hasValue(TEST_VALUE);
+        assertThat(buffer.getUpstream().get(key)).isPresent().hasValue(value);
 
-        buffer.getUpstream().remove(TEST_KEY);
-        assertThat(buffer.get(TEST_KEY)).isNotPresent();
-        assertThat(buffer.getUpstream().get(TEST_KEY)).isNotPresent();
+        buffer.getUpstream().remove(key);
+        assertThat(buffer.get(key)).isNotPresent();
+        assertThat(buffer.getUpstream().get(key)).isNotPresent();
 
     }
 
-    @Test
-    void testCommit() {
-        commitTrack.put(TEST_KEY, TEST_VALUE);
-        assertThat(commitTrack.get(TEST_KEY)).isPresent();
-        assertThat(commitTrack.get(TEST_KEY)).hasValue(TEST_VALUE);
-        assertThat(commitTrack.getUpstream().get(TEST_KEY)).isNotPresent();
+    @ParameterizedTest
+    @MethodSource("keyValueArgumentsProvider")
+    void testCommit(String key, String value) {
+        commitTrack.put(key, value);
+        assertThat(commitTrack.get(key)).isPresent().hasValue(value);
+        assertThat(commitTrack.getUpstream().get(key)).isNotPresent();
 
         observer.commit();
-        assertThat(commitTrack.get(TEST_KEY)).isPresent();
-        assertThat(commitTrack.getUpstream().get(TEST_KEY)).isPresent();
-        assertThat(commitTrack.getUpstream().get(TEST_KEY)).hasValue(TEST_VALUE);
+        assertThat(commitTrack.get(key)).isPresent();
+        assertThat(commitTrack.getUpstream().get(key)).isPresent().hasValue(value);
 
-        commitTrack.getUpstream().remove(TEST_KEY);
-        assertThat(commitTrack.get(TEST_KEY)).isNotPresent();
-        assertThat(commitTrack.getUpstream().get(TEST_KEY)).isNotPresent();
-        assertThat(buffer.getUpstream().get(TEST_KEY)).isNotPresent();
+        commitTrack.getUpstream().remove(key);
+        assertThat(commitTrack.get(key)).isNotPresent();
+        assertThat(commitTrack.getUpstream().get(key)).isNotPresent();
+        assertThat(buffer.getUpstream().get(key)).isNotPresent();
     }
 }
