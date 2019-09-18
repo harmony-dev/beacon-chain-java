29245fb2e1b20ac5ef1abb4c18eaec0630ca62e0
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-09-18 22:22:17 +0300

    test: delete directories

--- db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
@@ -1,18 +1,21 @@
 package org.ethereum.beacon.db;
 
-import org.ethereum.beacon.db.source.DataSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.rocksdb.RocksDBException;
-import tech.pegasys.artemis.util.bytes.BytesValue;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DatabaseTest {
@@ -34,17 +37,30 @@ class DatabaseTest {
   @MethodSource("validArgumentsProvider")
   void testValidCreation(String path, long bufferLimitsInBytes) throws IOException {
     final Database database = Database.rocksDB(path, bufferLimitsInBytes);
-    final DataSource<BytesValue, BytesValue> storage = database.createStorage("test-db");
-    storage.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
-    database.commit();
+    database.createStorage("test-db");
+    final Path directory = Paths.get(path);
+    assertThat(Files.exists(directory)).isTrue();
 
-    Files.deleteIfExists(Paths.get(path));
+    Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
   }
 
   private static Stream<Arguments> validArgumentsProvider() {
     return Stream.of(
             Arguments.of("/tmp/rocksdb", 0),
-            Arguments.of("/tmp/rocksdb", 1),
-            Arguments.of("/tmp/rocksdb", -1));
+            Arguments.of("/tmp/rocksdb1", Long.MAX_VALUE),
+            Arguments.of("/tmp/rocksdb2", Long.MIN_VALUE));
   }
 }
