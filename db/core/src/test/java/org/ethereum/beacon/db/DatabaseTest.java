23994a6dcf92898fa1eaca49deb099c17289d9c9
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-09-18 21:59:47 +0300

    test: fix DatabaseTest after code review

--- db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
@@ -1,6 +1,6 @@
 package org.ethereum.beacon.db;
 
-import org.ethereum.beacon.db.rocksdb.RocksDbSource;
+import org.ethereum.beacon.db.source.DataSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,25 +8,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.rocksdb.RocksDBException;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DatabaseTest {
 
-  @ParameterizedTest
-  @MethodSource("invalidArgumentsProvider")
+  @Test
   void testInvalidCreation() {
-    final EngineDrivenDatabase database = (EngineDrivenDatabase) Database.rocksDB("", 0);
-    final RocksDbSource upstream = (RocksDbSource) database.getWriteBuffer().getUpstream();
-    assertThatThrownBy(upstream::open)
+    final Database database = Database.rocksDB("", 0);
+    assertThatThrownBy(() -> database.createStorage("test-db"))
             .isInstanceOf(RuntimeException.class)
             .hasCauseExactlyInstanceOf(RocksDBException.class);
-  }
-
-  private static Stream<Arguments> invalidArgumentsProvider() {
-    return Stream.of(Arguments.of("", 0), Arguments.of("0123", 0));
   }
 
   @Test
@@ -36,20 +32,19 @@ class DatabaseTest {
 
   @ParameterizedTest
   @MethodSource("validArgumentsProvider")
-  void testValidCreation(String path, long bufferLimitsInBytes) {
-    final EngineDrivenDatabase database = (EngineDrivenDatabase) Database.rocksDB(path, bufferLimitsInBytes);
-    final RocksDbSource upstream = (RocksDbSource) database.getWriteBuffer().getUpstream();
-    upstream.open();
-    upstream.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
-    assertThat(database.getWriteBuffer().get(BytesValue.of(123))).isPresent().hasValue(BytesValue.of(1, 2, 3));
-    upstream.close();
+  void testValidCreation(String path, long bufferLimitsInBytes) throws IOException {
+    final Database database = Database.rocksDB(path, bufferLimitsInBytes);
+    final DataSource<BytesValue, BytesValue> storage = database.createStorage("test-db");
+    storage.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    database.commit();
+
+    Files.deleteIfExists(Paths.get(path));
   }
 
   private static Stream<Arguments> validArgumentsProvider() {
     return Stream.of(
-            Arguments.of("", 0),
-            Arguments.of("/tmp", 0),
-            Arguments.of("/tmp", Long.MAX_VALUE),
-            Arguments.of("/tmp", Long.MIN_VALUE));
+            Arguments.of("/tmp/rocksdb", 0),
+            Arguments.of("/tmp/rocksdb", 1),
+            Arguments.of("/tmp/rocksdb", -1));
   }
 }
