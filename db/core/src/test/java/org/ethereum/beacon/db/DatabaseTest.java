4fdb06d52171510294be9332259e0e52742ad5f2
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-09-18 20:47:37 +0300

    test: fix DatabaseTest after code review

--- db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
@@ -1,15 +1,13 @@
 package org.ethereum.beacon.db;
 
-import org.ethereum.beacon.db.source.DataSource;
-import org.ethereum.beacon.db.source.impl.HashMapDataSource;
-import org.junit.jupiter.api.Tag;
+import org.ethereum.beacon.db.rocksdb.RocksDbSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.rocksdb.RocksDBException;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 
-import java.nio.file.InvalidPathException;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,12 +15,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DatabaseTest {
 
-  @Tag("FIX")
   @ParameterizedTest
   @MethodSource("invalidArgumentsProvider")
-  void testInvalidCreation(String path, long bufferLimitsInBytes) {
-    assertThatThrownBy(() -> Database.rocksDB(path, bufferLimitsInBytes))
-            .isInstanceOf(InvalidPathException.class);
+  void testInvalidCreation() {
+    final EngineDrivenDatabase database = (EngineDrivenDatabase) Database.rocksDB("", 0);
+    final RocksDbSource upstream = (RocksDbSource) database.getWriteBuffer().getUpstream();
+    assertThatThrownBy(upstream::open)
+            .isInstanceOf(RuntimeException.class)
+            .hasCauseExactlyInstanceOf(RocksDBException.class);
+  }
+
+  private static Stream<Arguments> invalidArgumentsProvider() {
+    return Stream.of(Arguments.of("", 0), Arguments.of("0123", 0));
   }
 
   @Test
@@ -30,66 +34,22 @@ class DatabaseTest {
     assertThatThrownBy(() -> Database.rocksDB(null, 0)).isInstanceOf(NullPointerException.class);
   }
 
-  private static Stream<Arguments> invalidArgumentsProvider() {
-    return Stream.of(Arguments.of("null", 0), Arguments.of("", 0), Arguments.of("0123", 0));
-  }
-
   @ParameterizedTest
   @MethodSource("validArgumentsProvider")
   void testValidCreation(String path, long bufferLimitsInBytes) {
-    final Database database = Database.rocksDB(path, bufferLimitsInBytes);
-    assertThat(database).isNotNull();
+    final EngineDrivenDatabase database = (EngineDrivenDatabase) Database.rocksDB(path, bufferLimitsInBytes);
+    final RocksDbSource upstream = (RocksDbSource) database.getWriteBuffer().getUpstream();
+    upstream.open();
+    upstream.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    assertThat(database.getWriteBuffer().get(BytesValue.of(123))).isPresent().hasValue(BytesValue.of(1, 2, 3));
+    upstream.close();
   }
 
   private static Stream<Arguments> validArgumentsProvider() {
     return Stream.of(
+            Arguments.of("", 0),
             Arguments.of("/tmp", 0),
             Arguments.of("/tmp", Long.MAX_VALUE),
             Arguments.of("/tmp", Long.MIN_VALUE));
-  }
-
-  private static final String TEST_STORAGE_NAME = "TEST_STORAGE_NAME";
-
-  private boolean committed;
-  private boolean closed;
-  private String storageName;
-
-  @Test
-  void testDatabaseCommitCloseCreateStorage() {
-    final DataSource<BytesValue, BytesValue> dataSource = new HashMapDataSource<>();
-    committed = false;
-    closed = false;
-    storageName = null;
-
-    final Database db =
-            new Database() {
-              @Override
-              public DataSource<BytesValue, BytesValue> createStorage(String name) {
-                storageName = name;
-                return dataSource;
-              }
-
-              @Override
-              public void commit() {
-                committed = true;
-              }
-
-              @Override
-              public void close() {
-                closed = true;
-              }
-        };
-
-    final DataSource<BytesValue, BytesValue> storage = db.createStorage(TEST_STORAGE_NAME);
-    assertThat(storage).isEqualTo(dataSource);
-    assertThat(storageName).isEqualTo(TEST_STORAGE_NAME);
-
-    assertThat(committed).isFalse();
-    db.commit();
-    assertThat(committed).isTrue();
-
-    assertThat(closed).isFalse();
-    db.close();
-    assertThat(closed).isTrue();
   }
 }
