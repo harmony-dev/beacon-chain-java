b2a818656b193c82d18958189e9079d802a25f32
Eugene Ustimenko <evgenyustimenko@gmail.com>
2019-09-19 09:28:22 +0300

    test: add additional test for Database

--- db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
+++ db/core/src/test/java/org/ethereum/beacon/db/DatabaseTest.java
@@ -1,11 +1,13 @@
 package org.ethereum.beacon.db;
 
+import org.ethereum.beacon.db.source.DataSource;
 import org.ethereum.beacon.db.util.FileUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.rocksdb.RocksDBException;
+import tech.pegasys.artemis.util.bytes.BytesValue;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -35,9 +37,16 @@ class DatabaseTest {
   @MethodSource("validArgumentsProvider")
   void testValidCreation(String path, long bufferLimitsInBytes) throws IOException {
     final Database database = Database.rocksDB(path, bufferLimitsInBytes);
-    database.createStorage("test-db");
+    final DataSource<BytesValue, BytesValue> storage = database.createStorage("test-db");
     final Path directory = Paths.get(path);
     assertThat(Files.exists(directory)).isTrue();
+
+    //TODO: how to test without casting
+    storage.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    database.commit();
+    storage.remove(BytesValue.of(123));
+    database.commit();
+    database.close();
 
     FileUtil.removeRecursively("rocksdb");
   }
@@ -47,5 +56,39 @@ class DatabaseTest {
             Arguments.of("rocksdb", 0),
             Arguments.of("rocksdb2", Long.MAX_VALUE),
             Arguments.of("rocksdb3", Long.MIN_VALUE));
+  }
+
+  @Test
+  void testCreateDifferentStoresForSameDb() throws IOException {
+    final Database database = Database.rocksDB("rocksdb", Long.MAX_VALUE);
+    final DataSource<BytesValue, BytesValue> s1 = database.createStorage("test-db");
+    final DataSource<BytesValue, BytesValue> s2 = database.createStorage("test-db2");
+    assertThat(Files.exists(Paths.get("rocksdb"))).isTrue();
+
+    s1.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    s2.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    database.commit();
+    database.close();
+    s1.remove(BytesValue.of(123));
+    s2.remove(BytesValue.of(123));
+    assertThatThrownBy(database::commit).isInstanceOf(Exception.class); //TODO: should not commit after close
+
+    FileUtil.removeRecursively("rocksdb");
+  }
+
+  @Test
+  void testCreateDuplicateStorage() throws IOException {
+    final Database database = Database.rocksDB("rocksdb", Long.MAX_VALUE);
+    final DataSource<BytesValue, BytesValue> s1 = database.createStorage("test-db");
+    final DataSource<BytesValue, BytesValue> s2 = database.createStorage("test-db");
+    assertThat(Files.exists(Paths.get("rocksdb"))).isTrue();
+
+    //TODO: how to test without casting
+    s1.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    s2.put(BytesValue.of(123), BytesValue.of(1, 2, 3));
+    database.commit();
+    database.close();
+
+    FileUtil.removeRecursively("rocksdb");
   }
 }
