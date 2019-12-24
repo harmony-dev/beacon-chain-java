1a90cd561402565f0f12824dcf44a5b433128389
Alex Vlasov/ericsson49 <avv49@mail.ru>
2019-12-24 23:19:07 +0300

    additional parse/serialize methods implemented

--- test/src/test/java/org/ethereum/beacon/test/StateTestUtils.java
+++ test/src/test/java/org/ethereum/beacon/test/StateTestUtils.java
@@ -29,6 +29,7 @@ import org.ethereum.beacon.core.types.Time;
 import org.ethereum.beacon.core.types.ValidatorIndex;
 import org.ethereum.beacon.test.type.model.BeaconStateData;
 import org.ethereum.beacon.test.type.model.BlockData;
+import org.jetbrains.annotations.NotNull;
 import tech.pegasys.artemis.ethereum.core.Hash32;
 import tech.pegasys.artemis.util.bytes.Bytes32;
 import tech.pegasys.artemis.util.bytes.Bytes4;
@@ -36,6 +37,7 @@ import tech.pegasys.artemis.util.bytes.Bytes96;
 import tech.pegasys.artemis.util.bytes.BytesValue;
 import tech.pegasys.artemis.util.collections.Bitlist;
 import tech.pegasys.artemis.util.collections.Bitvector;
+import tech.pegasys.artemis.util.collections.ReadList;
 import tech.pegasys.artemis.util.collections.WriteList;
 import tech.pegasys.artemis.util.uint.UInt64;
 
@@ -287,4 +289,237 @@ public abstract class StateTestUtils {
             ? BLSSignature.wrap(Bytes96.fromHexString(data.getSignature()))
             : BLSSignature.ZERO);
   }
+
+  @NotNull
+  public static Attestation parseAttestation(BeaconStateData.AttestationData data, SpecConstants constants) {
+    BytesValue aggValue = BytesValue.fromHexString(data.getAggregationBits());
+    return new Attestation(
+        Bitlist.of(aggValue, constants.getMaxValidatorsPerCommittee().getValue()),
+        parseAttestationData(data.getData()),
+        data.getSignature() != null
+            ? BLSSignature.wrap(Bytes96.fromHexString(data.getSignature()))
+            : BLSSignature.ZERO,
+        constants);
+  }
+
+  public static BeaconStateData serializeBeaconState(BeaconState state) {
+    BeaconStateData data = new BeaconStateData();
+
+    data.setSlot(state.getSlot().toStringNumber(null));
+    data.setGenesisTime(state.getGenesisTime().longValue());
+    data.setFork(serializeFork(state.getFork()));
+
+    data.setPreviousJustifiedCheckpoint(serializeCheckpoint(state.getPreviousJustifiedCheckpoint()));
+    data.setCurrentJustifiedCheckpoint(serializeCheckpoint(state.getCurrentJustifiedCheckpoint()));
+    data.setJustificationBits(state.getJustificationBits().toString());
+    data.setFinalizedCheckpoint(serializeCheckpoint(state.getFinalizedCheckpoint()));
+    data.setLatestBlockHeader(serializeBeaconBlockHeader(state.getLatestBlockHeader()));
+    data.setEth1Data(serializeEth1Data(state.getEth1Data()));
+    data.setEth1DataVotes(state.getEth1DataVotes().stream().map(StateTestUtils::serializeEth1Data).collect(Collectors.toList()));
+    data.setEth1DepositIndex(state.getEth1DepositIndex().getValue());
+
+    data.setValidators(serializeValidatorRegistry(state.getValidators()));
+    data.setBalances(serializeBalances(state.getBalances()));
+    //data.setRandaoMixes(
+    //    state.getRandaoMixes().stream().map(Hash32::toString).collect(Collectors.toList()));
+    data.setPreviousEpochAttestations(serializePendingAttestations(state.getPreviousEpochAttestations()));
+    data.setCurrentEpochAttestations(serializePendingAttestations(state.getCurrentEpochAttestations()));
+    //data.setSlashings(serializeBalances(state.getSlashings()));
+
+    return data;
+  }
+
+  public static List<BeaconStateData.ValidatorData> serializeValidatorRegistry(ReadList<?,ValidatorRecord> validators) {
+    return validators.stream().map(StateTestUtils::serializeValidatorRecord).collect(Collectors.toList());
+  }
+
+  public static BeaconStateData.ValidatorData serializeValidatorRecord(ValidatorRecord record) {
+    BeaconStateData.ValidatorData data = new BeaconStateData.ValidatorData();
+
+    data.setPubkey(record.getPubKey().toHexString());
+    data.setWithdrawalCredentials(record.getWithdrawalCredentials().toString());
+    data.setEffectiveBalance(record.getEffectiveBalance().toString());
+    data.setSlashed(record.getSlashed());
+    data.setActivationEligibilityEpoch(record.getActivationEligibilityEpoch().toString());
+    data.setActivationEpoch(record.getActivationEpoch().toString());
+    data.setExitEpoch(record.getExitEpoch().toString());
+    data.setWithdrawableEpoch(record.getWithdrawableEpoch().toString());
+
+    return data;
+  }
+
+  public static List<BeaconStateData.AttestationData> serializePendingAttestations(
+      ReadList<?,PendingAttestation> attestations) {
+    return attestations.stream().map(StateTestUtils::serializePendingAttestation).collect(Collectors.toList());
+  }
+
+  public static BeaconStateData.AttestationData serializePendingAttestation(
+      PendingAttestation attestation) {
+    BeaconStateData.AttestationData data = new BeaconStateData.AttestationData();
+
+    data.setAggregationBits(serializeBitlist(attestation.getAggregationBits()));
+    data.setData(serializeAttestationData(attestation.getData()));
+    data.setInclusionDelay(attestation.getInclusionDelay().toStringNumber(null));
+    data.setProposerIndex(attestation.getProposerIndex().getValue());
+
+    return data;
+  }
+
+  public static BeaconStateData.AttestationData.AttestationDataContainer serializeAttestationData(AttestationData attestationData) {
+    BeaconStateData.AttestationData.AttestationDataContainer dataContainer = new BeaconStateData.AttestationData.AttestationDataContainer();
+
+    dataContainer.setSlot(attestationData.getSlot().getValue());
+    dataContainer.setIndex(attestationData.getIndex().getValue());
+    dataContainer.setBeaconBlockRoot(attestationData.getBeaconBlockRoot().toString());
+    dataContainer.setSource(serializeCheckpoint(attestationData.getSource()));
+    dataContainer.setTarget(serializeCheckpoint(attestationData.getTarget()));
+
+    return dataContainer;
+  }
+
+  public static List<String> serializeBalances(ReadList<?, Gwei> balances) {
+    return balances.stream().map(Gwei::toString).collect(Collectors.toList());
+  }
+
+  public static BeaconStateData.Fork serializeFork(Fork fork) {
+    BeaconStateData.Fork data = new BeaconStateData.Fork();
+    data.setPreviousVersion(fork.getPreviousVersion().toString());
+    data.setCurrentVersion(fork.getCurrentVersion().toString());
+    data.setEpoch(fork.getEpoch().toString());
+    return data;
+  }
+
+  public static BeaconStateData.CheckpointData serializeCheckpoint(Checkpoint checkpoint) {
+    BeaconStateData.CheckpointData data = new BeaconStateData.CheckpointData();
+    data.setEpoch(checkpoint.getEpoch().longValue());
+    data.setRoot(checkpoint.getRoot().toString());
+    return data;
+  }
+
+  public static BlockData.BlockBodyData.Eth1 serializeEth1Data(Eth1Data eth1Data) {
+    BlockData.BlockBodyData.Eth1 eth1 = new BlockData.BlockBodyData.Eth1();
+    eth1.setBlockHash(eth1Data.getBlockHash().toString());
+    eth1.setDepositCount(eth1Data.getDepositCount().toString());
+    eth1.setDepositRoot(eth1Data.getDepositRoot().toString());
+    return eth1;
+  }
+
+  public static BeaconStateData.BlockHeaderData serializeBeaconBlockHeader(BeaconBlockHeader header) {
+    BeaconStateData.BlockHeaderData data = new BeaconStateData.BlockHeaderData();
+    data.setSlot(header.getSlot().toStringNumber(null));
+    data.setParentRoot(header.getParentRoot().toString());
+    data.setStateRoot(header.getStateRoot().toString());
+    data.setParentRoot(header.getBodyRoot().toString());
+    data.setSignature(header.getSignature().toHexString());
+    return data;
+  }
+
+  public static BlockData serializeBlock(BeaconBlock block) {
+    BlockData.BlockBodyData blockBodyData = new BlockData.BlockBodyData();
+    blockBodyData.setRandaoReveal(block.getBody().getRandaoReveal().toHexString());
+    blockBodyData.setEth1Data(serializeEth1Data(block.getBody().getEth1Data()));
+    blockBodyData.setGraffiti(block.getBody().getGraffiti().toString());
+    blockBodyData.setProposerSlashings(
+        block.getBody().getProposerSlashings().stream()
+            .map(StateTestUtils::serializeProposerSlashing)
+            .collect(Collectors.toList()));
+    blockBodyData.setAttesterSlashings(
+        block.getBody().getAttesterSlashings().stream()
+            .map(StateTestUtils::serializeAttesterSlashing)
+            .collect(Collectors.toList()));
+    blockBodyData.setAttestations(
+        block.getBody().getAttestations().stream()
+            .map(StateTestUtils::serializeAttestation)
+            .collect(Collectors.toList()));
+    blockBodyData.setDeposits(
+        block.getBody().getDeposits().stream()
+            .map(StateTestUtils::serializeDeposit)
+            .collect(Collectors.toList()));
+    blockBodyData.setVoluntaryExits(
+        block.getBody().getVoluntaryExits().stream()
+            .map(StateTestUtils::serializeVoluntaryExit)
+            .collect(Collectors.toList()));
+
+    BlockData blockData = new BlockData();
+    blockData.setSlot(block.getSlot().toStringNumber(null));
+    blockData.setParentRoot(block.getParentRoot().toString());
+    blockData.setStateRoot(block.getStateRoot().toString());
+    blockData.setBody(blockBodyData);
+    blockData.setSignature(block.getSignature().toHexString());
+
+    return blockData;
+  }
+
+  @NotNull
+  public static BeaconStateData.AttestationData serializeAttestation(Attestation attestation) {
+    BeaconStateData.AttestationData data = new BeaconStateData.AttestationData();
+
+    data.setAggregationBits(serializeBitlist(attestation.getAggregationBits()));
+    data.setData(serializeAttestationData(attestation.getData()));
+    data.setSignature(attestation.getSignature().toHexString());
+
+    return data;
+  }
+
+  public static String serializeBitlist(Bitlist bits) {
+    List<Integer> bitList = bits.getBits();
+    bitList.add(bits.size());
+    return Bitlist.of(bits.size() + 1, bitList, Math.max(bits.maxSize(), bits.size() + 1)).copy().toString();
+  }
+
+  @NotNull
+  public static BlockData.BlockBodyData.AttesterSlashingData serializeAttesterSlashing(AttesterSlashing attesterSlashing) {
+    BlockData.BlockBodyData.AttesterSlashingData data = new BlockData.BlockBodyData.AttesterSlashingData();
+    data.setSlashableAttestation1(serializeSlashableAttestation(attesterSlashing.getAttestation1()));
+    data.setSlashableAttestation2(serializeSlashableAttestation(attesterSlashing.getAttestation2()));
+    return data;
+  }
+
+  @NotNull
+  public static BlockData.BlockBodyData.ProposerSlashingData serializeProposerSlashing(ProposerSlashing proposerSlashing) {
+    BeaconStateData.BlockHeaderData blockHeaderData1 = serializeBeaconBlockHeader(proposerSlashing.getHeader1());
+    BeaconStateData.BlockHeaderData blockHeaderData2 = serializeBeaconBlockHeader(proposerSlashing.getHeader2());
+    BlockData.BlockBodyData.ProposerSlashingData proposerSlashingData = new BlockData.BlockBodyData.ProposerSlashingData();
+    proposerSlashingData.setProposerIndex(proposerSlashing.getProposerIndex().getValue());
+    proposerSlashingData.setHeader1(blockHeaderData1);
+    proposerSlashingData.setHeader2(blockHeaderData2);
+    return proposerSlashingData;
+  }
+
+  public static BlockData.BlockBodyData.VoluntaryExitData serializeVoluntaryExit(
+      VoluntaryExit voluntaryExit) {
+    BlockData.BlockBodyData.VoluntaryExitData data = new BlockData.BlockBodyData.VoluntaryExitData();
+
+    data.setEpoch(voluntaryExit.getEpoch().toString());
+    data.setValidatorIndex(voluntaryExit.getValidatorIndex().getValue());
+    data.setSignature(voluntaryExit.getSignature().toHexString());
+
+    return data;
+  }
+
+  public static BlockData.BlockBodyData.DepositData serializeDeposit(Deposit deposit) {
+    BlockData.BlockBodyData.DepositData data = new BlockData.BlockBodyData.DepositData();
+
+    data.setProof(deposit.getProof().stream().map(Object::toString).collect(Collectors.toList()));
+    BlockData.BlockBodyData.DepositData.DepositDataContainer dataContainer = new BlockData.BlockBodyData.DepositData.DepositDataContainer();
+    dataContainer.setPubkey(deposit.getData().getPubKey().toHexString());
+    dataContainer.setWithdrawalCredentials(deposit.getData().getWithdrawalCredentials().toString());
+    dataContainer.setAmount(deposit.getData().getAmount().toString());
+    dataContainer.setSignature(deposit.getData().getSignature().toHexString());
+    data.setData(dataContainer);
+
+    return data;
+  }
+
+  public static BlockData.BlockBodyData.IndexedAttestationData serializeSlashableAttestation(
+      IndexedAttestation attestation) {
+    BlockData.BlockBodyData.IndexedAttestationData data = new BlockData.BlockBodyData.IndexedAttestationData();
+
+    data.setAttestingIndices(attestation.getAttestingIndices().stream().map(UInt64::getValue).collect(Collectors.toList()));
+    data.setData(serializeAttestationData(attestation.getData()));
+    data.setAggregateSignature(attestation.getSignature().toHexString());
+
+    return data;
+  }
+
 }
